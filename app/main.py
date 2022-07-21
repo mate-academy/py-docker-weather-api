729ac7a9bd8bca3d1daaeb9624143c03abd2dc5d
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-07-21 17:46:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+import os
+import requests
+
+key = os.environ["API_WEATHER_KEY"]
+
+
 def get_weather():
-    # write your code here
-    pass
+    print(key)
+    url = f"https://api.weatherapi.com/v1/current.json?key={key}&q=Paris&aqi=no"
+    response = requests.get(url)
+    jdata = response.json()
+    print(f"Temperature in Paris is {jdata['current']['temp_c']} C")
 
 
 if __name__ == "__main__":

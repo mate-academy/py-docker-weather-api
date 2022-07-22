99997560dc8f48c8037d2bb4f372c93205a64b8e
Олег Белый <belochard@gmail.com>
2022-07-22 10:43:08 +0300

    deleted api_key and added -alpine

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ import os
 
 
 def get_weather():
-    os.environ["API_KEY"] = "b45afc4e7b2a4bf9a02151752222107"
     api_key = os.environ.get("API_KEY")
     request_data = requests.get(f"https://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris").json()
     pprint(request_data)

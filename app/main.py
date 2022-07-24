6e6174017cb8329febf3cc12ff9328357bf95950
Evgeniy Tretiak <genoktretiack@gmail.com>
2022-07-24 21:18:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+import os
+import requests
+
+
 def get_weather():
-    # write your code here
-    pass
+    key = os.getenv("API_KEY")
+    data = requests.get(f"http://api.weatherapi.com/v1/current.json?key={key}&q=Paris").json()
+
+    print(f"Paris, {data['location']['localtime']} "
+          f"Temperature: {data['current']['temp_c']} C°, "
+          f"{data['current']['condition']['text']}")
 
 
 if __name__ == "__main__":

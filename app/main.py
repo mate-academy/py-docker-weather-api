c2235192717056dca36390eaa335c4949b44b1dd
Олег Белый <belochard@gmail.com>
2022-07-28 10:43:37 +0300

    normalized output

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 import requests
 
-from pprint import pprint
-
 import os
 
 
 def get_weather():
     api_key = os.environ.get("API_KEY")
     request_data = requests.get(f"https://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris").json()
-    pprint(request_data)
+    print("The weather in Paris is: \n"
+          f"{request_data['location']['name']}/"
+          f"{request_data['location']['country']}"
+          f" {request_data['location']['localtime']}"
+          f" Weather: {request_data['current']['feelslike_c']} Celsium,"
+          f" {request_data['current']['condition']['text']}")
 
 
 if __name__ == "__main__":

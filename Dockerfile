f02cde7230b12bcaba01ce05b0ec8ec56b0f473e
Andrei Korotenko <korotenko.qndroid@gmail.com>
2022-09-20 14:48:01 +0300

    Solution.v2

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR weather_api/
 
+RUN python -m pip install --upgrade pip
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
--- app/main.py
+++ app/main.py
@@ -2,15 +2,19 @@ import requests
 import os
 
 
+KEY = os.environ["API_KEY"]
+CITY = "Paris"
+URL = f"https://api.weatherapi.com/v1/current.json?key={KEY}&q={CITY}"
+
+
 def get_weather():
-    key = os.environ["API_KEY"]
-    city = "Paris"
-    url = f"https://api.weatherapi.com/v1/current.json?key={key}&q={city}"
-    response_json = requests.get(url).json()
+    response_json = requests.get(URL).json()
     location = f"{response_json['location']['name']}/{response_json['location']['country']}"
     local_time = response_json['location']["localtime"]
+
     weather = f"Weather: {response_json['current']['temp_c']} Celsius," \
               f" {response_json['current']['condition']['text']}"
+
     print(f"{location} {local_time} {weather}")
     return response_json
 

d2a3b420653a64cfd6891c45203d4207f36ba69a
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-10-26 14:58:10 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,14 +4,22 @@ import requests
 from dotenv import load_dotenv
 
 
+API_URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
     load_dotenv()
+
     key = os.environ.get("API_KEY")
+    city = "Paris"
+    parameters = {"key": {key}, "q": {city}}
+
     response = requests.get(
-        "http://api.weatherapi.com/v1/current.json",
-        params={"key": {key}, "q": "Paris"}
+        API_URL,
+        params=parameters
     )
     info = response.json()
+
     print(
         f"{info['location']['country']}/{info['location']['name']} "
         f"{info['location']['localtime']} "

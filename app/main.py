9125c653db00843a43f940262ae01a798f240c9b
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-10-27 11:32:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,29 +3,31 @@ import os
 import requests
 from dotenv import load_dotenv
 
+load_dotenv()
 
+KEY = os.environ.get("API_KEY")
+CITY = "Paris"
 API_URL = "http://api.weatherapi.com/v1/current.json"
 
 
 def get_weather() -> None:
-    load_dotenv()
-
-    key = os.environ.get("API_KEY")
-    city = "Paris"
-    parameters = {"key": {key}, "q": {city}}
 
     response = requests.get(
         API_URL,
-        params=parameters
+        params={"key": {KEY}, "q": {CITY}}
     )
-    info = response.json()
 
-    print(
-        f"{info['location']['country']}/{info['location']['name']} "
-        f"{info['location']['localtime']} "
-        f"Weather: {info['current']['temp_c']} Celsius, "
-        f"{info['current']['condition']['text']}"
-    )
+    if response.status_code == 200:
+        info = response.json()
+
+        print(
+            f"{info['location']['country']}/{info['location']['name']} "
+            f"{info['location']['localtime']} "
+            f"Weather: {info['current']['temp_c']} Celsius, "
+            f"{info['current']['condition']['text']}"
+        )
+
+    print("You need to check your API_URL, KEY and CITY")
 
 
 if __name__ == "__main__":

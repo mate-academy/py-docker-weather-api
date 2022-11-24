9581c621855df21436b25f5a5c910f855989b18d
HStakhiv <gsstahiv@gmail.com>
2022-11-24 23:16:42 +0200

    Preparation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+
+import requests
+
+BASE_URL = "http://api.weatherapi.com/v1/current.json?key="
+CITY = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = BASE_URL + API_KEY + "&q=" + CITY
+    result = requests.get(url)
+
+    if result.status_code == 200:
+        response = result.json()
+
+        for key, values in response.items():
+            print(f"{str(key).upper()}:")
+
+            for value_key, value in values.items():
+                print(f"    {value_key}: {value}")
+
+    else:
+        print("Can't access HTTP request")
 
 
 if __name__ == "__main__":

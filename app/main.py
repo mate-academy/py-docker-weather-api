674fc6eb0fdaccd839120f6474ff3cc3bdddc0f1
igor kosianenko <funkcionalkos@gmail.com>
2022-11-25 23:55:29 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -4,19 +4,22 @@ import os
 import requests
 import urllib.parse
 
-
+URL = "http://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
 
 
 def get_weather() -> None:
-    URL = "http://api.weatherapi.com/v1/current.json?"
-    FILTERING = "Paris"
-
     params = {
         "key": os.environ.get("API_KEY"),
         "q": FILTERING,
     }
 
-    print(json.dumps(requests.get(url=URL + urllib.parse.urlencode(params)).json(), indent=2))
+    print(
+        json.dumps(
+            requests.get(url=URL + urllib.parse.urlencode(params)).json(),
+            indent=2,
+        )
+    )
 
 
 if __name__ == "__main__":

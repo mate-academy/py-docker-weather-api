6b8349221c6074b7df03ea2237cb5858cf450f01
Alexander <alexzubkoff123@gmail.com>
2022-11-28 01:00:29 +0200

    Added env variable to Dockerfile and read in main.py API key

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.10.4-alpine
 LABEL maintainer="alexzubkoff123@gmail.com"
 
 ENV PYTHONUNBUFFERED 1
+ENV API_KEY=8b89884a56104151964200013222711
 
 WORKDIR app/
 
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import requests
+import os
 
 
 def get_weather() -> None:
+    api_key = os.environ["API_KEY"]
+    print(f"Running with API key: {api_key}, weather in Paris now (JSON):")
     weather_in_paris_current = requests.get(
         "http://api.weatherapi.com/v1/current.json?"
-        "key=8b89884a56104151964200013222711&q=Paris"
+        f"key={api_key}&q=Paris"
     )
     print(weather_in_paris_current.text)
 

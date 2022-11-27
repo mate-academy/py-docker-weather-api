1fa96310ff232cf4bb997d103533c7ff003b07bf
Alexander <alexzubkoff123@gmail.com>
2022-11-27 22:21:46 +0200

    Refactored according pep8

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ import requests
 
 
 def get_weather() -> None:
-    weather_in_paris_current = requests.get("http://api.weatherapi.com/v1/current.json?"
-                                    "key=8b89884a56104151964200013222711&q=Paris")
+    weather_in_paris_current = requests.get(
+        "http://api.weatherapi.com/v1/current.json?"
+        "key=8b89884a56104151964200013222711&q=Paris"
+    )
     print(weather_in_paris_current.text)
 
 

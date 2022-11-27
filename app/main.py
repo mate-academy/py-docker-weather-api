8d8dd268e688cc89e78d967d72308ea627c697c0
Alexander <alexzubkoff123@gmail.com>
2022-11-27 22:15:36 +0200

    Created dockerfile for python weather api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    weather_in_paris_current = requests.get("http://api.weatherapi.com/v1/current.json?"
+                                    "key=8b89884a56104151964200013222711&q=Paris")
+    print(weather_in_paris_current.text)
 
 
 if __name__ == "__main__":

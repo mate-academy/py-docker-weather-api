fb20bca510a4affab0b807bd30addf27d2bcc5da
Roman Kolmakov <cerne313@gmail.com>
2022-11-28 17:41:42 +0200

    Solve the task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import os
+import requests
+from requests import ConnectionError
+
+API_ENDPOINT = "http://api.weatherapi.com/v1/current.json"
+API_KEY = os.environ.get("API_KEY")
+CITY = "Odesa"
+
+
+def get_full_url(endpoint: str, api_key: str, city: str) -> str:
+    return f"{endpoint}?key={api_key}&q={city}&aqi=no"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    try:
+        res = requests.get(get_full_url(API_ENDPOINT, API_KEY, CITY)).json()
+
+        location = res["location"]
+        current = res["current"]
+
+        print(
+            f"And now for the weather in {location['region']}, {location['country']}:\n"
+            f"Temperature is {current['temp_c']} Celsius. "
+            f"Feels like {current['feelslike_c']} degrees.\n"
+            f"It's {(current['condition']['text'].lower())} now.\n"
+            f"Wind speed is {current['wind_kph']} km/h."
+        )
+
+    except ConnectionError:
+        print("Something went wrong with your connection. Try again later.")
 
 
 if __name__ == "__main__":

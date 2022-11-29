8f540668b08b177c431d55d56730f12642623e83
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-11-29 09:58:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import os
+
+import requests
+
+
+API_KEY = os.environ.get("API_KEY")
+WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "PARIS"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    request_get = requests.get(
+        url=WEATHER_API_URL,
+        params={"key": API_KEY, "q": "Paris"}
+    )
+
+    if request_get.status_code == 200:
+        print(f"Performing request to Weather API for city {CITY}...")
+
+        result = request_get.json()
+        location = (f"{result['location']['name']}/"
+                    f"{result['location']['country']}")
+        localtime = result["location"]["localtime"]
+        temp_c = result["current"]["temp_c"]
+        condition = result["current"]["condition"]["text"]
+
+        print(f"{location} {localtime} Weather: {temp_c} Celsius, {condition}")
+
+    else:
+        print("Invalid request")
 
 
 if __name__ == "__main__":

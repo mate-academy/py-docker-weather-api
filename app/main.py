d58f040944f915f1f656c3523b6690b9d0f4f780
stoveprofi <stoveprofi@gmail.com>
2022-12-06 12:17:13 +0000

    Solution. Implemented weather api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import requests as requests
+
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+API_KEY="271b353fbbe744ddb39102709220512"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    try:
+        result = requests.get(URL, params={'q': FILTERING, 'key': API_KEY})
+        data = result.json()
+        print(f'{data["location"]["name"]}/{data["location"]["country"]} '
+              f'{data["location"]["localtime"]} '
+              f'Weather: {data["current"]["temp_c"]} Celsius, '
+              f'{data["current"]["condition"]["text"]}')
+
+    except Exception as e:
+        print("Exception (weather):", e)
+        pass
 
 
 if __name__ == "__main__":

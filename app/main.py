c1bf2e71d8dae7622487792338ece76790daea8f
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-12-22 20:00:28 +0200

    Create Docker-Weather-API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import requests
+import os
+
+URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(f"{URL}?key={API_KEY}&q={CITY}&aqi=no").json()
+    city = response["location"]["name"]
+    country = response["location"]["country"]
+    localtime = response["location"]["localtime"]
+    temp = response["current"]["temp_c"]
+    condition = response["current"]["condition"]["text"]
+    print(
+        f"{city}/{country} {localtime} Weather: {temp} Celsius, {condition}"
+    )
 
 
 if __name__ == "__main__":

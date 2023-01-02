c0b779e87bedc3ca394e2036fb90f11b17a22518
AndriyKy <akrechovetskiy@gmail.com>
2023-01-02 14:14:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,41 @@
+import os
+import requests
+from prettytable import PrettyTable
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    URL = "https://api.weatherapi.com/v1/current.json?"
+    FILTERING = "Paris"
+    payload = {
+        "q": FILTERING,
+        "key": os.environ["API_KEY"]
+    }
+
+    result = requests.get(URL, params=payload).json()
+    location = result["location"]
+    current = result["current"]
+
+    pretty = PrettyTable(
+        ["LOCATION", f"{location['name']}/{location['country']}"]
+    )
+    pretty.add_rows(
+        [
+            ["Temperature", f"{current['temp_c']}°C | {current['temp_f']}°F"],
+            [
+                "Feels like",
+                f"{current['feelslike_c']}°C | {current['feelslike_f']}°F"
+            ],
+            ["Condition", current["condition"]["text"]],
+            [
+                "Wind speed",
+                f"{current['wind_mph']} mph | {current['wind_kph']} kph"
+            ],
+            ["Humidity", f"{current['humidity']}%"]
+        ]
+    )
+
+    print(f"Weather forcast for {location['localtime']}")
+    print(pretty)
 
 
 if __name__ == "__main__":

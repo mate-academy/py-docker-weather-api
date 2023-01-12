fb39cd0704bad6dd7d2f9cfec64f531d7aeaa16a
Volodymyr Panov <vpanov25@gmail.com>
2023-01-12 17:59:12 +0100

    olution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import requests
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    weatherapi = os.environ["weatherapi_key"]
+    city_to_find = "Paris"
+
+    url = f"https://api.weatherapi.com/v1/current.json?key=" \
+          f"{weatherapi}&q={city_to_find}&aqi=no"
+    data = requests.get(url)
+    data = data.json()
+    city = data["location"]["name"]
+    country = data["location"]["country"]
+    temperature = data["current"]["temp_c"]
+    weather = data["current"]["condition"]["text"]
+    local_time = data["location"]["localtime"]
+    print(f"{city}/{country} {local_time} "
+          f"Weather: {temperature} Celsius, {weather}")
 
 
 if __name__ == "__main__":

78dc2f8ce18e1e60ca2adb729f38325da03f01d8
George <georgepavlej@gmail.com>
2023-02-18 21:51:30 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise ValueError("API_KEY environment variable not set")
+
+URL = "https://api.weatherapi.com/v1/current.json?"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(URL + f"key={API_KEY}" + "&" + f"q={CITY}")
+    weather_data = response.json()
+    location = weather_data["location"]
+    current = weather_data["current"]
+    message = f"{location['name']}/{location['country']} " \
+              f"{location['localtime']} " \
+              f"Weather: {current['temp_c']}, " \
+              f"{current['condition']['text']}"
+    print(f"Performing request to Weather API for city {CITY}")
+    print(message)
 
 
 if __name__ == "__main__":

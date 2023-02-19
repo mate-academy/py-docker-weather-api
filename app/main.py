7400dc42d5b74cd4a3951e2ded046058bced4b79
George <georgepavlej@gmail.com>
2023-02-19 20:24:08 +0000

    Changed backslash to parenthesis in the get_weather()

--- app/main.py
+++ app/main.py
@@ -7,22 +7,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
-if not API_KEY:
-    raise ValueError("API_KEY environment variable not set")
 
 URL = "https://api.weatherapi.com/v1/current.json?"
 CITY = "Paris"
 
 
 def get_weather() -> None:
+    if not API_KEY:
+        raise ValueError("API_KEY environment variable not set")
     response = requests.get(URL + f"key={API_KEY}" + "&" + f"q={CITY}")
     weather_data = response.json()
     location = weather_data["location"]
     current = weather_data["current"]
-    message = f"{location['name']}/{location['country']} " \
-              f"{location['localtime']} " \
-              f"Weather: {current['temp_c']}, " \
-              f"{current['condition']['text']}"
+    message = (f"{location['name']}/{location['country']}"
+               f"{location['localtime']}"
+               f"Weather: {current['temp_c']},"
+               f"{current['condition']['text']}")
     print(f"Performing request to Weather API for city {CITY}")
     print(message)
 

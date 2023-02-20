5a265d117cece6e7e7bb9d15600d8ca9afd24459
Anton Chumak <Chumak7377@gmail.com>
2023-02-20 19:13:31 +0200

    py-docker-weather-api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
+import requests
+from decouple import config
+
+API_KEY = config("API_KEY")
+URL = "https://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = f"{URL}key={API_KEY}&q={FILTERING}&aqi=yes"
+    rq = requests.get(url)
+    with open("weather.html", "w") as output_file:
+        output_file.write(rq.text)
 
 
 if __name__ == "__main__":

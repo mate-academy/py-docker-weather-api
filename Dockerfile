d2f59b4d11c077751d29984b9708fece7e7d1363
Tetiana Omelchenko <t.omel4enko@gmail.com>
2023-03-07 18:42:18 +0800

    Solution

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.10-alpine
+LABEL maintainer=t.omel4enko@gmail.com
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+api_key = os.environ.get("API_KEY")
+WEATHER_URL = "https://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(
+        WEATHER_URL,
+        params=[("key", api_key), ("q", "Paris")]
+    )
+    if response.status_code == 200:
+        data = response.json()
+        current_weather = data["current"]
+        temperature = current_weather.get("temp_c")
+        humidity = current_weather.get("humidity")
+        pressure = current_weather.get("pressure_mb")
+        cloud = current_weather.get("cloud")
+        last_updated = current_weather.get("last_updated").split(" ")
+        print("Weather in Paris")
+        print(f"Temperature: {temperature}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Pressure: {pressure} mbar")
+        print(f"Cloud cover: {cloud}%")
+        print(f"Last updated {last_updated[0]} at {last_updated[1]}")
+    else:
+        print("Error in the HTTP request")
 
 
 if __name__ == "__main__":

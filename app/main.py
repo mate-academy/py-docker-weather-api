9c662f2469431729af48ffd6cd8c3fa6c657b7e0
Anna Kabatova <kabatova.anna@ukr.net>
2023-03-14 17:59:23 +0200

    Prepared for building docker image

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+API_KEY = os.getenv("API_KEY")
+PARAMS = {"key": API_KEY, "q": FILTERING}
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    result = requests.get(url=URL, params=PARAMS).json()
+
+    location = result["location"]
+    current = result["current"]
+    print(
+        f"Country: {location['country']}\n"
+        f"City: {location['name']}\n"
+        f"lat: {location['lat']}, long: {location['lon']}\n"
+        f"Time zone: {location['tz_id']}\n"
+        f"Local time: {location['localtime']}\n"
+    )
+    print(
+        f"Temperature: {current['temp_c']} C\n"
+        f"Condition: {current['condition']['text']}\n"
+        f"Wind speed: {current['wind_kph']} km/h\n"
+        f"Wind direction: {current['wind_dir']}\n"
+        f"Pressure: {current['pressure_mb']} mb\n"
+        f"Humidity: {current['humidity']}%"
+    )
 
 
 if __name__ == "__main__":

34c1562132a840e02eb89628de01713910015ce9
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-03-16 16:42:12 +0100

    Add .dockerignore along with Dockerfile and rewrite get_weather func

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10.4-slim
+LABEL maintaner="maggielit"
+
+ENV PYTHONBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+COPY . .
+CMD ["python","app/main.py","get_weather"]
--- app/main.py
+++ app/main.py
@@ -11,19 +11,16 @@ CITY = "Paris"
 
 
 def get_weather() -> None:
-    response = requests.get(URL + f"key={API_KEY}&q={CITY}")
-    if response.status_code == 200:
-        result = response.json()
-        print(f"Performing request for Weather Api for city {CITY}...")
-        sleep(1)
-        print(
-            f'{CITY}/{result["location"]["country"]}'
-            f' {result["location"]["localtime"]}'
-            f' Weather: {result["current"]["temp_c"]}'
-            f' Celsius, {result["current"]["condition"]["text"]}'
-        )
-    else:
-        print("your API_KEY is wrong or Weather site unavailable")
+    response = requests.get(URL, params={"key": API_KEY, "q": CITY})
+    result = response.json()
+    print(f"Performing request for Weather Api for city {CITY}...")
+    sleep(1)
+    print(
+        f'{CITY}/{result["location"]["country"]}'
+        f' {result["location"]["localtime"]}'
+        f' Weather: {result["current"]["temp_c"]}'
+        f' Celsius, {result["current"]["condition"]["text"]}'
+    )
 
 
 if __name__ == "__main__":

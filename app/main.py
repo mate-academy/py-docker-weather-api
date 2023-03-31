3dcba6ad082f57fc7215af29033236d4b6a1352b
Іван Березянський <mamanadarin@gmail.com>
2023-03-31 13:31:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,17 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 URL = "http://api.weatherapi.com/v1/current.json"
-API_KEY = os.getenv("API_KEY")
-FILTERING = "paris"
+FILTERING = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+PARAMS = f"?key={API_KEY}&q={FILTERING}"
 
 
 def get_weather() -> None:
-    weather_response = requests.get(URL + f"?key={API_KEY}&q={FILTERING}")
-    temperature_celsius = weather_response.json()["current"]["temp_c"]
-    location_name = weather_response.json()["location"]["name"]
-    location_region = weather_response.json()["location"]["country"]
-    location_time = weather_response.json()["location"]["localtime"]
-    condition_text = weather_response.json()["current"]["condition"]["text"]
+    weather_response = requests.get(f"{URL}{PARAMS}")
+    w_rj = weather_response.json()
+
+    temperature_celsius = w_rj["current"]["temp_c"]
+    location_name = w_rj["location"]["name"]
+    location_region = w_rj["location"]["country"]
+    location_time = w_rj["location"]["localtime"]
+    condition_text = w_rj["current"]["condition"]["text"]
     result = f"{location_name}/{location_region} {location_time} " \
              f"Weather: {temperature_celsius} Celsius, {condition_text}"
     print(result)

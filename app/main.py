8098bf6276ad232a4b4b9730be251f84b27189e2
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-01 15:09:38 +0300

    Add response status code checking to main.py and empty line to Dockerfile

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("API_Key")
+API_KEY = os.getenv("API_KEY")
 URL = "https://api.weatherapi.com/v1/current.json"
 
 
@@ -13,15 +13,20 @@ def get_weather() -> None:
     response = requests.get(
         url=URL,
         params={"key": API_KEY, "q": "Paris"}
-    ).json()
+    )
+    response_json = response.json()
 
-    current_temperature = response["current"]["temp_c"]
-    current_pressure = response["current"]["pressure_mb"]
-    current_humidity = response["current"]["humidity"]
+    if response.status_code != 200:
+        print("something went wrong ")
 
-    print(f"Temperature (in celsius) = {current_temperature} "
-          f"\nAtmospheric pressure (in millibars) = {current_pressure} "
-          f"\nHumidity (in percentage) = {current_humidity}")
+    else:
+        current_temperature = response_json["current"]["temp_c"]
+        current_pressure = response_json["current"]["pressure_mb"]
+        current_humidity = response_json["current"]["humidity"]
+
+        print(f"Temperature (in celsius) = {current_temperature} "
+              f"\nAtmospheric pressure (in millibars) = {current_pressure} "
+              f"\nHumidity (in percentage) = {current_humidity}")
 
 
 if __name__ == "__main__":

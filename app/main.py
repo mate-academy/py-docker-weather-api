54a30ffbca1943414bc5fe9254a95c2c5bb6085e
Roman Senyshyn <senjaskorp@gmail.com>
2023-04-10 05:46:03 +0300

    1 commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    CITY = "Paris"
+    API_KEY = os.environ["API_KEY"]
+    URL = "https://api.weatherapi.com/v1/current.json"
+    response = requests.get(
+        f"{URL}?key={API_KEY}&q={CITY}&aqi=no")
+    data = response.json()
+    location = data["location"]
+    current = data["current"]
+    print(f"Performing request to Weather API for city {CITY}...")
+    print(
+        (
+            f"{location['country']}/"
+            f"{location['name']} "
+            f"{location['localtime']} "
+            f"Weather: {current['temp_c']} "
+            f"Celsius, {current['condition']['text']}"
+        )
+    )
 
 
 if __name__ == "__main__":

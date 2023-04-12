b27d37194d58d386c930ebe134b7430c3b8bf016
usrofgh <mailforgimail@gmail.com>
2023-04-12 19:01:02 +0300

    Soludion

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+
+WEATHER_API = os.environ.get("WEATHER_API")
+
+
+URL = f"https://api.weatherapi.com/v1/current.json?key={WEATHER_API}"
+FILTER_CITY = "Paris"
+URL_WITH_PARAMS = URL + f"&q={FILTER_CITY}"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    answer = requests.get(URL_WITH_PARAMS).json()
+    country = answer["location"]["country"]
+    city = answer["location"]["name"]
+    datetime_ = answer["location"]["localtime"]
+    temp_c = answer["current"]["temp_c"]
+    condition = answer["current"]["condition"]["text"]
+
+    res = (
+        f"{city}/{country} "
+        f"{datetime_} "
+        f"Weather: {temp_c} "
+        f"Celsius, {condition}"
+    )
+    print(res)
 
 
 if __name__ == "__main__":

fbaf96c7797fcb1854e6804f4bd346c0322a142e
Dmytry <donchykd@gmail.com>
2023-04-12 16:19:15 +0300

    Configuring app for docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+import os
+
+import requests
+
+API_KEY = os.getenv("API_KEY")
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = "https://api.weather.com/v1/current.json"
+
+    params = {"key": API_KEY, "q": FILTERING}
+
+    response = requests.get(url, params=params)
+
+    print(response.content)
 
 
 if __name__ == "__main__":

6c1be3d72ac8823e4f771ce588084dc14ac26ba5
David Gluschenko <davidglusch@gmail.com>
2023-04-13 15:32:20 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+import requests
+import os
+
+WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
+API_KEY = os.getenv("API_KEY")
+
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    payload = {"key": API_KEY, "q": "SW1"}
+
+    response = requests.get(WEATHER_API_URL, payload)
+
+    print(response.content)
 
 
 if __name__ == "__main__":

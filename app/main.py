12f6d0c4e85a3842edcc09013a12140030a7da6f
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-13 16:42:33 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -2,17 +2,20 @@ import requests
 import os
 
 
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    URL = "https://api.weatherapi.com/v1/current.json"
-    FILTERING = "Paris"
     print("Performing request to Weather API for city Paris...")
-    api_key = os.getenv('API_KEY')
-    payload = {'key': api_key, 'q': FILTERING}
+    api_key = os.getenv("API_KEY")
+    payload = {"key": api_key, "q": FILTERING}
     response = requests.get(URL, params=payload)
     location = response.json()["location"]
     data = response.json()["current"]
-    weather = (f"{location['name']}/{location['country']} {data['last_updated']} "
-               f"Weather: {data['temp_c']} Celsius, {data['condition']['text']}")
+    weather = (f"{location['name']}/{location['country']} "
+               f"{data['last_updated']} Weather: {data['temp_c']} Celsius, "
+               f"{data['condition']['text']}")
     print(weather)
 
 

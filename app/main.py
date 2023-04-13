830e11b6ee46a9686ef18c77856ec3bfe1e49635
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-13 21:55:35 +0300

    Changed code according to flake8

--- app/main.py
+++ app/main.py
@@ -4,35 +4,28 @@ import requests
 
 
 def get_weather() -> None:
-    # Define the API endpoint and parameters
     url = "https://api.weatherapi.com/v1/current.json"
     api_key = os.environ.get("API_KEY")
     params = {
-        "key": api_key,  # Replace with your WeatherAPI API key
-        "q": "Paris",  # Specify the location as Paris
+        "key": api_key,
+        "q": "Paris",
     }
 
-    # Send a GET request to the WeatherAPI
     response = requests.get(url, params=params)
-    # print(response.status_code)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Parse the JSON response
         data = response.json()
 
-        # Extract the relevant information
         city = data["location"]["name"]
         country = data["location"]["country"]
         temperature = data["current"]["temp_c"]
         condition = data["current"]["condition"]["text"]
 
-        # Print the current weather information
         print(f"Current weather in {city}, {country}:")
         print(f"Temperature: {temperature}°C")
         print(f"Condition: {condition}")
     else:
-        print("Failed to retrieve weather data. Please check your API key and try again.")
+        print("Failed to retrieve weather data.")
 
 
 if __name__ == "__main__":

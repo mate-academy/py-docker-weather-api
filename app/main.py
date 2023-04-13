99d6a40ee65f630303f9c74ef30c454b7c4fe775
ostapix@gmail.com <@Bandera345>
2023-04-13 21:45:19 +0300

    Revert "Solution"
    This reverts commit 6514b68e62608921d2f2822fa3cc1e11dab927b2.

--- app/main.py
+++ app/main.py
@@ -1,22 +1,6 @@
-import os
-
-import requests
-
-API_KEY = os.environ.get("API_KEY")
-
-URL = f"http://api.weatherapi.com/v1/current.json?ket={API_KEY}&"
-FILTERING = "Paris"
-
-
 def get_weather() -> None:
-    parameters = {
-        "key": API_KEY,
-        "q": FILTERING
-    }
-
-    output = requests.get(URL, params=parameters)
-
-    print(output.content)
+    # write your code here
+    pass
 
 
 if __name__ == "__main__":

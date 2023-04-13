2a0fe089f71818d8d7d2277b9606bb076000ffa8
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-04-13 17:19:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import os
+
+import requests
+from typing import Dict
+
+API_KEY = os.environ["API_KEY"]
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    URl = "https://api.weatherapi.com/v1/current.json"
+
+    params = {"key": API_KEY, "q": FILTERING}
+
+    response = requests.get(URl, params=params)
+
+    if response.status_code == 200:
+        data: Dict = response.json()
+        temp_c = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        print(f"Current weather in {FILTERING}: {temp_c}°C, {condition}")
+    else:
+        print("Failed to fetch weather data")
 
 
 if __name__ == "__main__":

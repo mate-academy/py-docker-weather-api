fc4d28579294898413860c531d05dcb121652645
Oleksandr Leha <legus96@gmail.com>
2023-04-17 20:09:06 +0300

    Solution_key_fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import os
 import requests
 
-API_KEY = "c4c72a4109d14453bd0133710231304"
+API_KEY = os.getenv("API_KEY")
 CITY = "Paris"
 URL = "http://api.weatherapi.com/v1/current.json"
 PAYLOAD = {"key": API_KEY, "q": CITY}

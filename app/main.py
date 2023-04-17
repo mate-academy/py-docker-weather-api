bf131b17be620ce9fea4063771848a2d888845fe
Vadym Bihun <bihunva@gmail.com>
2023-04-17 17:48:12 +0300

    Remove errors handling

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ import os
 import requests
 
 API_KEY = os.environ.get("API_KEY")
-if not API_KEY:
-    raise ValueError("API_KEY not found in environment variables")
 
 WEATHER_API_URL = "https://api.weatherapi.com/v1/current.json"
 CITY = "Paris"
@@ -17,25 +15,18 @@ def get_weather() -> None:
     """
     print(f"Performing request to Weather API for city {CITY}...")
 
-    try:
-        params = {"key": API_KEY, "q": CITY}
-        response = requests.get(WEATHER_API_URL, params=params).json()
+    params = {"key": API_KEY, "q": CITY}
+    response = requests.get(WEATHER_API_URL, params=params).json()
 
-        country = response["location"]["country"]
-        _datetime = response["location"]["localtime"]
-        temperature = response["current"]["temp_c"]
-        condition = response["current"]["condition"]["text"]
+    country = response["location"]["country"]
+    _datetime = response["location"]["localtime"]
+    temperature = response["current"]["temp_c"]
+    condition = response["current"]["condition"]["text"]
 
-        print(
-            f"{country}/{CITY} {_datetime} "
-            f"Weather: {temperature} Celsius, {condition}"
-        )
-
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-
-    except requests.exceptions.RequestException as err:
-        print(f"An error occurred: {err}")
+    print(
+        f"{country}/{CITY} {_datetime} "
+        f"Weather: {temperature} Celsius, {condition}"
+    )
 
 
 if __name__ == "__main__":

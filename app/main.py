5b1c285f6813399f8a6489acae355b3c54890a39
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-18 14:41:32 +0300

    fix location

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ import os
 
 
 URL = "https://api.weatherapi.com/v1/current.json"
-FILTERING = "Paris"
+CITY = "Paris"
 
 
 def get_weather() -> None:
-    print("Performing request to Weather API for city Paris...")
+    print(f"Performing request to Weather API for {CITY}...")
     api_key = os.getenv("API_KEY")
-    payload = {"key": api_key, "q": FILTERING}
+    payload = {"key": api_key, "q": CITY}
     response = requests.get(URL, params=payload)
     location = response.json()["location"]
     data = response.json()["current"]

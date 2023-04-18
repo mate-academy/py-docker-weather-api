7f50ca79e679a8c3af2f51687890adc989642890
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-18 07:48:05 +0300

    remove try except block, update name of Filter variable

--- app/main.py
+++ app/main.py
@@ -1,33 +1,27 @@
 import os
 import requests
-from requests import HTTPError
 
 URL = "https://api.weatherapi.com/v1/current.json?"
-FILTERING = "Paris"
+CITY = "Paris"
 
 
 def get_weather() -> None:
-    try:
-        api_key = os.environ.get("API_KEY")
-        response = requests.get(
-            URL + f"key={api_key}" + f"&q={FILTERING}" + "&aqi=no"
-        )
-        weather_data_dict = response.json()
-        location_data = weather_data_dict.get("location")
-        temperature_data = weather_data_dict.get("current")
-        condition_data = temperature_data.get("condition")
-        result = (
-            f"{location_data['name']}/{location_data['country']} "
-            f"{location_data['localtime']} "
-            f"Weather: {temperature_data['temp_c']} "
-            f"Celsius, {condition_data['text']}"
-        )
-        print(f"Performing request to Weather API for city {FILTERING}...")
-        print(result)
-    except HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as other_err:
-        print(f"Other error occurred: {other_err}")
+    api_key = os.environ.get("API_KEY")
+    response = requests.get(
+        URL + f"key={api_key}" + f"&q={CITY}" + "&aqi=no"
+    )
+    weather_data_dict = response.json()
+    location_data = weather_data_dict.get("location")
+    temperature_data = weather_data_dict.get("current")
+    condition_data = temperature_data.get("condition")
+    result = (
+        f"{location_data['name']}/{location_data['country']} "
+        f"{location_data['localtime']} "
+        f"Weather: {temperature_data['temp_c']} "
+        f"Celsius, {condition_data['text']}"
+    )
+    print(f"Performing request to Weather API for city {CITY}...")
+    print(result)
 
 
 if __name__ == "__main__":

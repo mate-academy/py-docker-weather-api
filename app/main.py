fc2a4e79b9566109c5e33bec51fd8c52eeec6514
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-18 14:45:19 +0300

    fix API_KEY usage

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ import os
 
 URL = "https://api.weatherapi.com/v1/current.json"
 CITY = "Paris"
+API_KEY = os.getenv("API_KEY")
 
 
 def get_weather() -> None:
     print(f"Performing request to Weather API for {CITY}...")
-    api_key = os.getenv("API_KEY")
-    payload = {"key": api_key, "q": CITY}
+    payload = {"key": API_KEY, "q": CITY}
     response = requests.get(URL, params=payload)
     location = response.json()["location"]
     data = response.json()["current"]

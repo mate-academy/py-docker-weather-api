2c35d3eac592071d07ebd6a151cacbd02c4829da
manjustice <manjustice1998@gmail.com>
2023-04-19 13:53:15 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -4,17 +4,27 @@ import requests
 
 
 API_KEY = os.getenv("API_KEY")
-FILTERING = "Odesa"
+CITY = "Odesa"
 
 
 def get_weather() -> None:
     url = "https://api.weatherapi.com/v1/current.json"
 
-    params = {"key": API_KEY, "q": FILTERING}
+    params = {"key": API_KEY, "q": CITY}
 
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=params).json()
 
-    print(response.content)
+    city = response["location"]["name"]
+    country = response["location"]["country"]
+    date = response["location"]["localtime"]
+    temperature = response["current"]["temp_c"]
+    condition = response["current"]["condition"]["text"]
+
+    print(f"Performing request to Weather API for city {city}...")
+    print(
+        f"{city}/{country} {date} "
+        f"Weather: {temperature} Celsius, {condition}"
+    )
 
 
 if __name__ == "__main__":

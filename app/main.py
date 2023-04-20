da2a5fe9a89c70fdf67d32c0ea5167e4ef271003
name <ksyusha0kubay@gmail.com>
2023-04-20 08:58:23 +0300

    Add .dockerignore

--- app/main.py
+++ app/main.py
@@ -5,19 +5,23 @@ import requests
 
 URL = "http://api.weatherapi.com/v1/current.json"
 FILTERING = "Paris"
-API_KEY = os.environ.get("API_KEY")
 
 
 def get_weather() -> None:
-    response = requests.get(
-        f"{URL}?key={API_KEY}&q={FILTERING}&aqi=no")
+    params = {
+        "key": os.environ.get("API_KEY"),
+        "q": FILTERING,
+        "aqi": "no"
+    }
+    response = requests.get(URL, params=params)
+
     data = response.json()
     location = data["location"]
     current = data["current"]
     print(f"Performing request to Weather API for city {FILTERING}...")
     print(
         (
-            f"{location['country'] }"
+            f"{location['country']} "
             f"{location['name']} "
             f"{location['localtime']} "
             f"Weather: {current['temp_c']} "

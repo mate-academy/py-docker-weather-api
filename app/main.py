14ef4850a4952f407fd18730400e0dec79383e02
IZevsl <michael.lyvak@gmail.com>
2023-04-20 15:14:00 +0300

    Made response more user-friendly

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import requests
 
-
 API_KEY = "3647e2845af24914ad1111124231304"
 FILTERING = "Paris"
 URL = "https://api.weatherapi.com/v1/current.json"
@@ -14,8 +13,17 @@ def get_weather() -> None:
     }
 
     response = requests.get(URL, params=params)
+    info = response.json()
 
-    print(response.content)
+    city = info.get("location").get("name")
+    temperature = info.get("current").get("feelslike_c")
+    wind = info.get("current").get("wind_mph")
+
+    print(
+        f"In {city} weather for today: \n"
+        f"    Temperature | {temperature} C*; \n"
+        f"    Wind        | {wind} m/h;"
+    )
 
 
 if __name__ == "__main__":

3ab162e5b891cbaa9c23a8caad7e9d81e07205c7
Bohdan Kovalenko <bog7807@gmail.com>
2023-04-20 18:20:56 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import os
+import requests
+# 3e82112350fd46cd8d954441231404
+
+LOCATION = "Paris"
+API_KEY = os.getenv("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = "https://api.weatherapi.com/v1/current.json"
+
+    params = {
+        "key": API_KEY,
+        "q": LOCATION,
+    }
+
+    response = requests.get(url, params=params)
+
+    print(response.content)
 
 
 if __name__ == "__main__":

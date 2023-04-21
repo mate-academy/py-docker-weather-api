a66f258c047aceed904d75fc833df513cc7eff02
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-04-21 14:47:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -17,7 +17,8 @@ def get_weather() -> None:
     print(f"Performing request to Weather API for city {CITY}..."
           f"{res['location']['name']}/{res['location']['country']} "
           f"{res['location']['localtime']} Weather: "
-          f"{res['current']['temp_c']} Celsius, {res['current']['condition']['text']}")
+          f"{res['current']['temp_c']} Celsius, "
+          f"{res['current']['condition']['text']}")
 
 
 if __name__ == "__main__":

09cac0711d1175548bee1e3578c308087357fa6e
Artur Tytomyr <art.tytomyr@gmail.com>
2023-04-24 02:53:56 +0300

    revised

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ load_dotenv()
 
 def get_weather() -> str:
     key = os.environ.get("KEY")
-    FILTERING = "Paris"
-    URL = "http://api.weatherapi.com/v1/current.json?key="
-    response = requests.get(URL + f"{key}&q=" + f"{FILTERING}&api=no")
+    filtering = "Paris"
+    url = "http://api.weatherapi.com/v1/current.json?key="
+    response = requests.get(url + f"{key}&q=" + f"{filtering}&api=no")
     weather_data = response.json()
     temperature = weather_data.get("current")["temp_c"]
-    return f"The temperature in {FILTERING} is {temperature} degrees Celsius"
+    return f"The temperature in {filtering} is {temperature} degrees Celsius"
 
 
 if __name__ == "__main__":

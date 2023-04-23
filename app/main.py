5691d6e9d8f0f7575fdf45a0cdae034975c39017
Arsen Makovei <makoveyarsen@gmail.com>
2023-04-24 02:31:57 +0300

    Implemented the service for getting current weather for Paris city, dockerized and pushed to the dockerhub

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+URL = "https://api.weatherapi.com/v1/current.json"
+API_KEY = os.getenv("API_KEY")
+LOCATION = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {"key": API_KEY, "q": LOCATION}
+    response = requests.get(URL, params=params)
+    data = response.json()
+    print(json.dumps(data, indent=4))
 
 
 if __name__ == "__main__":

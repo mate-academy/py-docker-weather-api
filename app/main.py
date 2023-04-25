1e98aff0f1578e10a5b68a49420736550f440a6d
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-04-25 12:27:39 +0300

    v2 add block try, add newline to dockerignore

--- app/main.py
+++ app/main.py
@@ -7,23 +7,29 @@ CITY = "Paris"
 
 
 def get_weather() -> None:
-    response = requests.get(
-        url=URL,
-        params={
-            "key": API_KEY,
-            "q": CITY
-        }
-    ).json()
+    try:
+        response = requests.get(
+            url=URL,
+            params={
+                "key": API_KEY,
+                "q": CITY
+            }
+        )
+        response.raise_for_status()
+        data = response.json()
 
-    temp_c = int(response["current"]["temp_c"])
-    temp_f = int(response["current"]["temp_f"])
-    feelslike_c = int(response["current"]["feelslike_c"])
-    condition_text = response["current"]["condition"]["text"].lower()
+        temp_c = int(data["current"]["temp_c"])
+        temp_f = int(data["current"]["temp_f"])
+        feelslike_c = int(data["current"]["feelslike_c"])
+        condition_text = data["current"]["condition"]["text"].lower()
 
-    print(f"Greetings!\nI hasten to inform you that"
-          f" outside the window {condition_text} in Paris.\n"
-          f"The temperature is {temp_c} Celsius ({temp_f}F),"
-          f" feels like {feelslike_c}. Enjoy your stay!")
+        print(f"Greetings!\nI hasten to inform you that"
+              f" outside the window {condition_text} in Paris.\n"
+              f"The temperature is {temp_c} Celsius ({temp_f}F),"
+              f" feels like {feelslike_c}. Enjoy your stay!")
+
+    except requests.exceptions.HTTPError as error:
+        print(f"An HTTP error occurred: {error}")
 
 
 if __name__ == "__main__":

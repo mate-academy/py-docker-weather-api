13aeb4d5b3b13d549d024f27d5e4e53be5926add
David Gluschenko <davidglusch@gmail.com>
2023-05-03 18:56:16 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -3,14 +3,19 @@ import os
 
 WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
 API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
 
 
 def get_weather() -> None:
-    payload = {"key": API_KEY, "q": "SW1"}
+    payload = {"key": API_KEY, "q": CITY}
 
     response = requests.get(WEATHER_API_URL, payload)
 
-    print(response.content)
+    data = response.json()
+    print(f"Current weather for {CITY}:")
+    print(f"Temperature: {data['current']['temp_c']} C")
+    print(f"Weather condition: {data['current']['condition']['text']}")
+    print(f"Wind speed: {data['current']['wind_kph']} km/h")
 
 
 if __name__ == "__main__":

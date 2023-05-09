ee5115d63ce78c60458b9edeb4eb0af504379a1a
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-09 08:12:33 +0300

    Finish program implementation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    result = requests.get(
+        f"{URL}key={API_KEY}&q={FILTERING}"
+    ).json()
+
+    print(f"{result.get('location').get('name')}"
+          f"/{result.get('location').get('country')}"
+          f" {result.get('location').get('localtime')}"
+          f" Weather: {result.get('current').get('temp_c')} Celsius, "
+          f"{result.get('current').get('condition').get('text')}")
 
 
 if __name__ == "__main__":

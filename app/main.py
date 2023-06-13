a519614f7f3e30d97b205f59267f776b7afc08e8
Oleksandr Mamchur <irex1988@gmail.com>
2023-06-13 20:42:23 +0300

    fixed url

--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ load_dotenv()
 
 API_KEY = os.environ["WEATHER_API_KEY"]
 
+WEATHER_API_URL = "https://api.weatherapi.com/v1/current.json"
+
 
 def get_weather() -> None:
-    url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}=Paris"
-    response = requests.get(url)
+    payload = {"key": API_KEY, "q": "Paris"}
+    response = requests.get(WEATHER_API_URL, params=payload)
     print(response.text)
 
 

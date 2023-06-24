0a436e378a962da65800008d10b994b3822fc035
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-06-24 19:57:23 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11.2-slim
+LABEL maintainer="shulep@ukr.net"
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    city = "Paris"
+    api_key = os.getenv("API_KEY")
+
+    response = requests.get(
+        f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    )
+
+    if response.ok:
+        data = response.json()
+        country = data["location"]["country"]
+        current = data["current"]
+        date = current["last_updated"]
+        temperature = current["temp_c"]
+        condition = current["condition"]["text"]
+        print(
+            f"{city}/{country} {date} "
+            f"Weather: {temperature} Celsius, {condition}"
+        )
+
+    else:
+        print("Failed to retrieve weather information.")
 
 
 if __name__ == "__main__":

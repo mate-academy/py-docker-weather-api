e03734469b384ffafa42294f5402b1fe0822fc32
Ivan <ivan.korshunov.1997@gmail.com>
2023-07-03 18:33:54 +0300

    solution

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+#FROM python:3.11.0
+FROM python:3.11.0-alpine
+LABEL maintainer="ivan.korshunov.1997@gmail.com"
+# Who is responsible for this project.
+
+ENV PYTHONUNBUFFERED 1
+# Set this in order to see logs.
+
+WORKDIR app/
+# Path inside of the container --> directory app
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+# Copy py-docker-weather-api2 directory to
+# app/ directory in container
+
+#CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python3", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import os
+
+import requests
+
+
 def get_weather() -> None:
     # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    response = requests.get(
+        (f"https://api.weatherapi.com/v1/"
+         f"current.json?q=Paris&"
+         f"key={api_key}")
+    )
+
+    if response.status_code == 200:
+        weather_data = response.json()
+
+        location = weather_data["location"]["name"]
+        temperature = weather_data["current"]["temp_c"]
+        condition = weather_data["current"]["condition"]["text"]
+
+        print(
+            f"The current weather in {location} is:\n"
+            f"Temperature: {temperature} degrees Celsius\n"
+            f"Weather condition: {condition}"
+        )
+    elif response.status_code == 403:
+        print("Please enter API_KEY. Current API_KEY is not valid.")
+    else:
+        print("Error occurred while fetching weather data")
 
 
 if __name__ == "__main__":

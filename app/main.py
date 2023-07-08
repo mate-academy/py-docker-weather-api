39441b28d192807b1063fa608bf3d6e79677e378
Petro Abramov <sebshe3@gmail.com>
2023-07-09 00:36:57 +0300

    Added method get

--- app/main.py
+++ app/main.py
@@ -10,17 +10,23 @@ def get_weather() -> None:
     api_key = os.environ.get("API_KEY")
     url = (URL + f"key={api_key}&q={FILTERING}&lang=en")
 
-    result = requests.get(url)
+    response = requests.get(url)
+    result = response.json()
 
     if result.status_code != 200:
         print(f"Error: {result.status_code} - {result.reason}")
     else:
-        data = result.json()
-        location = data["location"]["name"]
-        temperature = data["current"]["temp_c"]
-        condition = data["current"]["condition"]["text"]
+        location = result.get("location")
+        temperature = result.get("current").get("temp_c")
+        condition = result.get("current").get("condition").get("text")
 
-        print(f"The weather in {location} is {temperature}°C and {condition}.")
+        if location and location.get("name"):
+            location = location.get("name")
+
+        if temperature and condition:
+            print(f"Weather in {location}: {temperature}°C, {condition}")
+        else:
+            print("Failed to fetch weather data")
 
 
 if __name__ == "__main__":

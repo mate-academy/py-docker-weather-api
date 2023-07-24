46bb706750ea9ade2fad072b7561ff8aeeb04fc7
ZaikaDaria <corplinebusiness@gmail.com>
2023-07-24 23:48:03 +0300

    Add solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv()
+BASIC_URL = os.getenv("BASIC_URL")
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(f"{BASIC_URL}/current.json", {
+        "key": API_KEY,
+        "q": CITY
+    })
+    if response.status_code == 200:
+        data = response.json()
+        condition = data["current"]["condition"]["text"]
+        humidity = data["current"]["humidity"]
+        temperature = data["current"]["temp_c"]
+        temp_last_update = data["current"]["last_updated"]
+
+        print(f"The weather in {CITY} is {condition.lower()}. "
+              f"The temperature is {temperature} Celsius, "
+              f"humidity - {humidity} "
+              f"(last update: {temp_last_update}).")
+    else:
+        print("Cannot retrieve information.")
 
 
 if __name__ == "__main__":

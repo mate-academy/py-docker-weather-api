01350865d5f23b7617ee49a2fdaa32dfcc1aad11
KirillMelanich <kirillmelanich@gmail.com>
2023-08-03 17:54:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import os
+import requests
+
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.environ.get("API_KEY")
+    url = (URL + f"key={api_key}&q={FILTERING}&lang=en")
+
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        print(f"Error: {res.status_code} - {res.reason}")
+    else:
+        data = res.json()
+        location = data["location"]["name"]
+        temperature = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+
+        print(f"The weather in {location} is {temperature}°C and {condition}.")
 
 
 if __name__ == "__main__":

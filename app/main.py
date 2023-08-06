b30b872058c0eb6463641f1a2d15fcfd9f59992c
Anton Komarov <anton_komarov_qa@ukr.net>
2023-08-06 19:33:52 +0300

    'Made constant variables and move them out the function'

--- app/main.py
+++ app/main.py
@@ -2,24 +2,26 @@ import os
 import requests
 
 
+CITY = "Paris"
+KEY = os.environ["API_KEY"]
+URL = f"https://api.weatherapi.com/v1/current.json?q={CITY}"
+
+
 def get_weather() -> None:
-    city = "Paris"
-    key = os.environ["API_KEY"]
-    url = f"https://api.weatherapi.com/v1/current.json?q={city}"
     headers = {
-        "key": key
+        "key": KEY,
     }
 
-    result = requests.get(url, headers=headers)
+    result = requests.get(URL, headers=headers)
 
     data = result.json()
     country = data["location"]["country"]
     last_updated = data["current"]["last_updated"]
     temp_c = data["current"]["temp_c"]
     weather_text = data["current"]["condition"]["text"]
-    result_string = (f"{city}/{country} {last_updated} "
+    result_string = (f"{CITY}/{country} {last_updated} "
                      f"Weather: {temp_c} Celsius, {weather_text}")
-    print(f"Performing request to Weather API for city {city}...")
+    print(f"Performing request to Weather API for city {CITY}...")
     print(result_string)
 
 

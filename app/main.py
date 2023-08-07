96b00f8b203357265a21f8180e2d80e65862fe20
Eugene Polunin <altahoma@gmail.com>
2023-08-08 01:08:03 +0300

    Created simple dockerized weather app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
+import os
+
+import requests
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": os.environ.get("API_KEY"),
+        "q": FILTERING,
+    }
+    response = requests.get(url=URL, params=params)
+    data = response.json()
+
+    country = data["location"]["country"]
+    time = data["location"]["localtime"]
+    temp_c = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(f"Performing request to Weather API for city {FILTERING}...")
+    print(
+        f"{country}/{FILTERING} {time} Weather: {temp_c} Celsius, {condition}"
+    )
 
 
 if __name__ == "__main__":

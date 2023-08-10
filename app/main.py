9443215fc0c5c8606ede50e614450fb669c44a5d
Erik Agayan <erik.agayan1@gmail.com>
2023-08-10 10:44:41 +0300

    Fix params

--- app/main.py
+++ app/main.py
@@ -4,25 +4,29 @@ import requests
 
 CITY = "Paris"
 KEY = os.environ["API_KEY"]
-URL = f"https://api.weatherapi.com/v1/current.json?q={CITY}"
+BASE_URL = "https://api.weatherapi.com/v1/current.json"
+
+params = {
+    "q": CITY,
+    "key": KEY
+}
 
 
 def get_weather() -> None:
-    key_value = {
-        "key": KEY,
-    }
+    response = requests.get(BASE_URL, params=params)
 
-    result = requests.get(URL, headers=key_value)
-
-    data_value = result.json()
-    country = data_value["location"]["country"]
-    last_data = data_value["current"]["last_updated"]
-    temperature = data_value["current"]["temp_c"]
-    weather = data_value["current"]["condition"]["text"]
-    result_string = (f"{CITY}/{country} {last_data} "
-                     f"Weather: {temperature} Celsius, {weather}")
-    print(f"Performing request to Weather API for city {CITY}...")
-    print(result_string)
+    if response.status_code == 200:
+        data_value = response.json()
+        country = data_value["location"]["country"]
+        last_data = data_value["current"]["last_updated"]
+        temperature = data_value["current"]["temp_c"]
+        weather = data_value["current"]["condition"]["text"]
+        result_string = (f"{CITY}/{country} {last_data} "
+                         f"Weather: {temperature} Celsius, {weather}")
+        print(f"Performing request to Weather API for city {CITY}...")
+        print(result_string)
+    else:
+        print("Error:", response.status_code)
 
 
 if __name__ == "__main__":

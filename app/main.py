30da6a22d08dec36f42e8984c0376cf27af75155
Volodymyr Bohomol <8773927@gmail.com>
2023-08-13 22:27:42 +0300

    Docker Weather API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import requests
+import json
+import os
+
+
+URL = "https://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    result = requests.get(URL + f"q={FILTERING}&key={API_KEY}")
+    result = json.loads(result.content)
+    if result.get("error"):
+        print(f"{result.get('error').get('message')}")
+        return
+    print(f"{result.get('location').get('tz_id')} "
+          f"{result.get('current').get('last_updated')} "
+          f"Weather: {result.get('current').get('temp_c')} Celsius",
+          f"{result.get('current').get('condition').get('text')}")
 
 
 if __name__ == "__main__":

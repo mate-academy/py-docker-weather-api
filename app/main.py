a47bd34cd43c16a80a9dab7c2a40804e02b3f452
Volodymyr Bohomol <8773927@gmail.com>
2023-08-14 11:42:28 +0300

    Fixes: - added `Performing request` message; - added default value to error result message - used [] in correct respond message

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import os
 
 
@@ -9,15 +8,15 @@ API_KEY = os.environ.get("API_KEY")
 
 
 def get_weather() -> None:
-    result = requests.get(URL + f"q={FILTERING}&key={API_KEY}")
-    result = json.loads(result.content)
+    print(f"Performing request to Weather API for city {FILTERING}...")
+    result = (requests.get(URL + f"q={FILTERING}&key={API_KEY}")).json()
     if result.get("error"):
-        print(f"{result.get('error').get('message')}")
+        print(f"{result.get('error').get('message', 'oops, something wrong')}")
         return
-    print(f"{result.get('location').get('tz_id')} "
-          f"{result.get('current').get('last_updated')} "
-          f"Weather: {result.get('current').get('temp_c')} Celsius",
-          f"{result.get('current').get('condition').get('text')}")
+    print(f"{result['location']['tz_id']} "
+          f"{result['current']['last_updated']} "
+          f"Weather: {result['current']['temp_c']} Celsius",
+          f"{result['current']['condition']['text']}")
 
 
 if __name__ == "__main__":

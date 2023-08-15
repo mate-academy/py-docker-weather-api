43790d32991dbf19785521bdf5860a966d8157dd
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-08-15 15:32:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,38 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+
+
+PARAMETERS = {
+    "key": API_KEY,
+    "q": CITY,
+}
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    response = requests.get(URL, params=PARAMETERS)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        print(f"Performing request to Weather API for city {CITY}")
+
+        city = data.get("location")["name"]
+        country = data.get("location")["country"]
+        localtime = data.get("location")["localtime"]
+        temperature = data.get("current")["temp_c"]
+        condition = data.get("current")["condition"]["text"]
+
+        print(f"{city}/{country} {localtime} Weather: {temperature}, {condition}")
+    else:
+        print(f"ERROR: {response.status_code}")
 
 
 if __name__ == "__main__":

e2ced3198ebd2a90b0e7802fdf21aa0ecbb1f441
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-08-27 10:49:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+URL = "https://api.weatherapi.com/v1/current.json"
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
+PARAMS = {
+    "key": API_KEY,
+    "q": CITY,
+}
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    request = requests.get(url=URL, params=PARAMS).json()
+
+    city = request.get("location")["name"]
+    country = request.get("location")["country"]
+    localtime = request.get("location")["localtime"]
+    temp = request.get("current")["temp_c"]
+    condition = request.get("current")["condition"]["text"]
+
+    weather_string = (
+        f"{city}/{country} {localtime} "
+        f"Weather: {temp} Celsius, {condition}"
+    )
+
+    print(weather_string)
 
 
 if __name__ == "__main__":

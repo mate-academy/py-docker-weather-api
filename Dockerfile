7a48b30ec7dc73fa7e1c5be5b97cfdebcfbd89e4
Munkoor <eee1lvlboss@gmail.com>
2023-09-30 22:50:17 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10.7
+
+ENV PYTHONNUNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+import os
+
+from requests import get
+import datetime
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    API_KEY = os.environ.get("API_KEY")
+    BASE_URL = "https://api.weatherapi.com/v1/current.json"
+    params = {"key": API_KEY, "q": "Paris"}
+
+    response = get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        print(
+            f"Paris/France {datetime.date.today()} Weather: {temperature}°C, {condition}")
+    else:
+        print(f"Weather data could not be obtained")
 
 
 if __name__ == "__main__":

715ccffbc34d0818191a85724bc5b47ed4045ac5
OlegatorLE <oleh.travin@gmail.com>
2023-10-17 15:51:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import datetime
+import os
+from requests import get
+
+
+API_KEY = os.getenv("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+PARAMS = {
+    "key": API_KEY,
+    "q": CITY,
+}
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    res = get(url=URL, params=PARAMS)
+
+    if res.status_code == 200:
+        weather_data = res.json()
+
+        city_datetime = weather_data["location"]["localtime"]
+        cur_temp = weather_data["current"]["temp_c"]
+        condition = weather_data["current"]["condition"]["text"]
+        print(f"Current weather in {CITY} at {city_datetime}: \n"
+              f"{cur_temp}°C \n"
+              f"{condition}")
 
 
 if __name__ == "__main__":

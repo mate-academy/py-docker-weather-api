a2367c1f1f64d0ea85cf04ff78060db746ba4e71
Yeva <yevabrookman@gmail.com>
2023-10-17 14:59:24 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ API_KEY = os.environ.get("API_KEY")
 def get_weather() -> None:
     response = requests.get(API_URL, params={"key": API_KEY, "q": "Paris"})
     print("Performing request to Weather API for city Paris...")
-    data_location = response.json()['location']
-    data_weather = response.json()['current']
+    data_location = response.json()["location"]
+    data_weather = response.json()["current"]
     print(f"{data_location['name']}/{data_location['country']} "  # noqa: Q000
           f"{data_location['localtime']} "  # noqa: Q000
           f"Weather: {data_weather['temp_c']} "  # noqa: Q000

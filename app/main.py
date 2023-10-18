641f712fc9f001588176a8ef96fdda7882ffce93
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-18 18:32:57 +0300

    Implement Weather API and dockerize it

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import requests
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    URL = "https://api.weatherapi.com/v1/current.json"
+    API_KEY = os.environ.get("API_KEY")
+    CITY = "Paris"
+    response = requests.get(URL, params={"key": API_KEY, "q": CITY})
+    response_json = response.json()
+
+    print(
+        f"{response_json['location']['name']}/"
+        f"{response_json['location']['country']} "
+        f"{response_json['location']['localtime']} "
+        f"Weather: {response_json['current']['temp_c']} Celsius, "
+        f"{response_json['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":

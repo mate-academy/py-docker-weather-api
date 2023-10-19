1531de33e7a492777ae82eaf2fffe382cb04d4c5
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-19 17:12:00 +0200

    implemented dockerized service

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11.3
+
+LABEL author="vikazaibuba@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,41 @@
+import os
+import requests
+
+API_URL = "https://api.weatherapi.com/v1/current.json"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not API_KEY:
+        print("API_KEY environment variable is not set.")
+        return
+
+    response = requests.get(API_URL, params={"key": API_KEY, "q": "Paris"})
+
+    if response.status_code != 200:
+        print(f"Failed to retrieve weather data. Status code: "
+              f"{response.status_code}")
+        return
+
+    response_data = response.json()
+
+    if "error" in response_data:
+        print(f"Error from Weather API: {response_data['error']['message']}")
+        return
+
+    location_info = response_data.get("location", {})
+    if location_info:
+        weather_info = response_data.get("current", {})
+        if weather_info:
+            print(f"{location_info['name']}/{location_info['country']} "
+                  f"{location_info['localtime']} "
+                  f"Weather: {weather_info.get('temp_c')} "
+                  f"Celsius, "
+                  f"{weather_info.get('condition', {}).get('text', '')}")
+        else:
+            print("Weather information not found in the response.")
+    else:
+        print("Location information not found in the response.")
 
 
 if __name__ == "__main__":

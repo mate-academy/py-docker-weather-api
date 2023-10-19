0df9e8465be82d957e34411c932d555a327a0a74
ORENYT <nkforworkmail@gmail.com>
2023-10-19 16:19:31 +0300

    Implemented weather API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    print(f"Performing request to Weather API for city Paris...")
+    api_key = os.environ.get("API_KEY")
+    if api_key is None:
+        raise Exception("API Key not found.")
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris"
+    response = requests.get(url)
+    if response.status_code == 200:
+        retrieve_result(response)
+    else:
+        raise Exception("Bad request. Check API key and WeatherAPI status")
+
+
+def retrieve_result(input):
+    data = input.json()
+    print(
+        f"{data['location']['name']}/{data['location']['country']} "
+        f"{data['location']['localtime']} "
+        f"Weather: {data['current']['temp_c']} Celsius, "
+        f"{data['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":

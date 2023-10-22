dffc83bb209994a3e8a0a0bec5a69b22fb8110b2
Illia <kononenko2109@gmail.com>
2023-10-22 18:48:55 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -3,20 +3,22 @@ import requests
 
 URL = "http://api.weatherapi.com/v1/current.json"
 FILTERING = "Paris"
+api_key = os.getenv("API_KEY")
 
 
 def get_weather() -> None:
-    api_key = os.getenv("API_KEY")
     payload = {"q": FILTERING, "key": api_key}
 
     request = requests.get(URL, params=payload)
     weather_data = request.json()
-
+    localtime = weather_data["location"]["localtime"]
+    temp_c = weather_data["current"]["temp_c"]
+    condition = weather_data["current"]["condition"]["text"]
     print("Performing request to Weather API for city Paris...")
     print(
-        f"Paris/France {weather_data['location']['localtime']} "
-        f"Weather: {weather_data['current']['temp_c']} Celsius, "
-        f"{weather_data['current']['condition']['text']}"
+        f"Paris/France {localtime} "
+        f"Weather: {temp_c} Celsius, "
+        f"{condition}"
     )
 
 

8dde6faef14ab3b6946e1c2dc4690e4bf1bd18e8
Sasha Semenenko <semenenko233249@gmail.com>
2023-10-24 21:56:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,43 @@
+import os
+
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(
+        "http://api.weatherapi.com/v1/current.json",
+        params={
+            "key": os.environ.get("API_KEY"),
+            "q": "Paris"
+        })
+
+    if response.status_code == 200:
+        data = response.json()
+
+        city = data["location"]["name"]
+        region = data["location"]["region"]
+        country = data["location"]["country"]
+        localtime = data["location"]["localtime"]
+        wind_kph = data["wind_kph"]
+        wind_dir = data["wind_dir"]
+        humidity = data["humidity"]
+        cloud = data["cloud"]
+
+        print(
+            f"City {city}"
+            f"Region {region}"
+            f"Country {country}"
+            f"Local Time {localtime}"
+            f"Wind speed in kilometer per hour {wind_kph}"
+            f"Wind direction {wind_dir}"
+            f"Humidity {humidity}"
+            f"Cloud cover {cloud}"
+        )
+    else:
+        print(
+            f"Your personal token is not valid"
+            f"Please, regenerate new and try again!"
+            f"Status code {response.status_code}")
 
 
 if __name__ == "__main__":

f467bff12481c905f923abdbc98b54364ff91c0d
Taras Savchyn <savik1992@gmail.com>
2023-10-26 22:05:56 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.10.8-slim
+LABEL maintainer="savik1992@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+ENV API_KEY NONE
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+import dotenv
+dotenv.load_dotenv()
+
+
+FILTERING = "Paris"
+
+API_KEY = os.getenv("API_KEY")
+
+URL = "https://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {"key": API_KEY, "q": FILTERING}
+    res = requests.get(URL, params=params).json()
+    if res.get("error") is not None:
+        print(res.get("error"))
+        return
+    print(
+        f"Country: {res.get('location').get('country')};\n"
+        f"City: {res.get('location').get('name')};\n"
+        f"Time: {res.get('location').get('localtime')};\n"
+        f"Weather: {res.get('current').get('condition').get('text')}\n"
+        f"Temperature: {res.get('current').get('temp_c')} Celsius;"
+    )
 
 
 if __name__ == "__main__":

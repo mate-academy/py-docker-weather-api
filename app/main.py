0e65a0cce711943995e5008ec9eb7a0623cdaf3c
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-10-31 17:58:43 +0200

    py-docker-weather-api

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def get_weather() -> None:
             f"{text}"
         )
     else:
-        print(f"Bad request: {resource.status_code}")
+        print(f"Bad request: {result.status_code}")
 
 
 if __name__ == "__main__":

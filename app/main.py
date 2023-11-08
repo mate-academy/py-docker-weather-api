2a85dcdf8eb9efd1be03c733522e540c300b40eb
Vlad Larin <lordgadyka71@gmail.com>
2023-11-08 20:25:32 +0200

    Update get_weather function to print current temperature in Paris

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = (
+        "http://api.weatherapi.com/v1/current.json"
+        "?key=a57a9bf8f7fa4865885175927230811&q=Paris"
+    )
+    res = requests.get(url)
+    if res.status_code == 200:
+        temp_c = res.json().get("current").get("temp_c")
+        temp_f = res.json().get("current").get("temp_f")
+        last_updated = res.json().get("current").get("last_updated")
+        print(
+            "Current temperature in Paris: "
+            f"{temp_c} C, {temp_f} F (last updated: {last_updated})"
+        )
+    else:
+        print(f"There is {res.status_code} error with your request")
 
 
 if __name__ == "__main__":

08be95ffa5192369a7a46754cd3be95c07098b22
Mazur Mariya <mazurinka29@gmail.com>
2023-11-12 01:08:29 +0200

    Solution of py-docker-weather-api

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.8.10-slim
+
+LABEL maintainer="mazurinka29@gmail.com"
+
+ENV  PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+
+CMD [ "python", "app/main.py", "0.0.0.0:8000"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import requests
+import os
+import json
+
+URL = "https://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    result_from_weather_api = requests.get(URL + f"q={FILTERING}&key={KEY}")
+
+    weather_dict = json.loads(result_from_weather_api.text)
+
+    print(f"Performing request to Weather API for city {FILTERING}...")
+    print(
+        f"{FILTERING}/{weather_dict['location']['country']}"
+        f" {weather_dict['location']['localtime']} "
+        f"Weather: {weather_dict['current']['temp_c']} Celsius,"
+        f" {weather_dict['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":

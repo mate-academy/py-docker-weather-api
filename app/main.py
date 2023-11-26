026d39d4f80808404d834b56f10ea3c41773e8cd
Sergii Pryima <spryima@gmail.com>
2023-11-26 14:30:50 +0200

    error handling added

--- app/main.py
+++ app/main.py
@@ -1,30 +1,38 @@
 import requests
 import os
-import json
+from requests.exceptions import RequestException
 
 
 URL = "https://api.weatherapi.com/v1/current.json?"
 API_KEY = os.getenv("API_KEY")
+
 FILTERING = "Paris"
 
 
 def get_weather() -> None:
-    weather = requests.get(URL + f"key={API_KEY}&q={FILTERING}")
+    try:
+        weather = requests.get(URL + f"key={API_KEY}&q={FILTERING}")
 
-    print(f"Performing request to Weather API for city {FILTERING}...")
+        if weather.status_code == 200:
 
-    data = json.loads(weather.text)
+            print(f"Performing request to Weather API for city {FILTERING}...")
+            data = weather.json()
 
-    print(
-        data["location"]["name"],
-        "/",
-        data["location"]["country"],
-        data["location"]["localtime"],
-        "Weather: ",
-        data["current"]["temp_c"],
-        "Celsius, ",
-        data["current"]["condition"]["text"],
-    )
+            print(
+                data["location"]["name"],
+                "/",
+                data["location"]["country"],
+                data["location"]["localtime"],
+                "Weather: ",
+                data["current"]["temp_c"],
+                "Celsius, ",
+                data["current"]["condition"]["text"],
+            )
+        else:
+            print(f"Error: {weather.status_code}")
+
+    except RequestException as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":

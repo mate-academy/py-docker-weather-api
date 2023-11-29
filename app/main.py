c74b1472c9253a497fe8bda7df020b069501070e
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-29 11:14:28 +0200

    Created pull request

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_URL = "https://api.weatherapi.com/v1/current.json"
+CITY_FILTERING = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = BASE_URL + f"?key={API_KEY}" + f"&q={CITY_FILTERING}"
+    res = requests.get(url)
+
+    if res.status_code == 200:
+        weather_dict = res.json()
+
+        location = weather_dict.get("location").get("name")
+        condition_text = weather_dict.get("current").get("condition").get("text")
+        last_updated = weather_dict.get("current").get("last_updated")
+        temp_c = weather_dict.get("current").get("temp_c")
+        humidity = weather_dict.get("current").get("humidity")
+
+        print(f"Location: {location}")
+        print(f"Condition: {condition_text}")
+        print(f"Last updated: {last_updated}")
+        print(f"Temperature: {temp_c} C")
+        print(f"Humidity: {humidity} %")
+    else:
+        print(f"There is {res.status_code} error with your request")
 
 
 if __name__ == "__main__":

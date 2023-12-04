fdbb97103563ddc51f2f6f0f6a91c2e05fbb22e0
Vitalii <vitalikkhmura132gmail.com>
2023-12-04 23:59:10 +0200

    Updated

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import os
 import requests
 
-URL = ("http://api.weatherapi.com/v1/current."
-       "json?key=9ed0ea20d5ea4cf2bbd224726230212&q=Paris&aqi=yes")
+URL = "https://api.weatherapi.com/v1/current.json"
 
 API_KEY = os.getenv("API_KEY")
+
 
 
 def get_weather() -> None:
@@ -22,8 +22,8 @@ def get_weather() -> None:
     condition = weather_data["current"]["condition"]["text"]
     print("Performing request to Weather API for city Paris...")
     print(
-        f"Paris/France {localtime} "
-        f"Weather: {temp_c} Celsius, "
+        f"Paris/France {localtime} \n"
+        f"Weather: {temp_c} Celsius, \n"
         f"{condition}"
     )
 

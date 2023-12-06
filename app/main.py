4d31014aaf88a3e6ab34046bed6cef6d12275838
Ihor Prochnickiy <2prochigor@gmail.com>
2023-12-06 17:54:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,38 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = os.environ["URL"]
+FILTERING = os.environ["FILTERING"]
+API_KEY = os.environ["API_KEY"]
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    params = {
+        "q": FILTERING,
+        "key": API_KEY
+    }
+
+    response = requests.get(URL, params=params)
+
+    if response.status_code != 200:
+        print(f"Request error, Status code: {response.status_code} != 200")
+        return
+
+    weather_data = response.json()
+    capital = weather_data["location"]["name"]
+    country = weather_data["location"]["country"]
+    localtime = weather_data["location"]["localtime"]
+    temperature = weather_data["current"]["temp_c"]
+    weather = weather_data["current"]["condition"]["text"]
+    print(
+        f"{capital}/{country} {localtime} "
+        f"Weather: {temperature} Celsius, {weather}"
+    )
 
 
 if __name__ == "__main__":

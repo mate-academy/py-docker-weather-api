25b331a2aafd7503ea110f4db466a6203bb25e30
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-07 16:08:24 +0200

    create wether app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
+def get_weather(
+        url: str = URL,
+        filtering: str = FILTERING,
+        api_key: str = API_KEY
+) -> None:
+    """Get weather from API and print it."""
+    params = {"key": api_key, "q": filtering}
+    res = requests.get(url, params=params).json()
+
+    city = res["location"]["name"]
+    country = res["location"]["country"]
+    temp = res["current"]["temp_c"]
+    condition = res["current"]["condition"]["text"]
+
+    print(
+        f"""
+        City: {city}
+        Country: {country}
+        Temperature: {temp}°C
+        Condition: {condition}
+        """
+    )
 
 
 if __name__ == "__main__":

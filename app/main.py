00e61c37deb9f4070fe7e54f75e4eaed281d641b
Denys <den.prislipskyi@gmail.com>
2023-12-07 22:49:22 +0100

    docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
+URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {"key": API_KEY, "q": CITY}
+
+    response = requests.get(URL, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        print(f"Current Weather in {CITY}:")
+        print(f"Weather: {weather_data['current']['condition']['text']}")
+        print(f"Temperature: {weather_data['current']['temp_c']} Celsius")
+    else:
+        print("Failed to fetch weather data.")
 
 
 if __name__ == "__main__":

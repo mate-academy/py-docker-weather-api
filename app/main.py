49691b8b500c1160741d0f275e14b9f12a43c87f
Kostia <kostia3507@gmail.com>
2023-12-09 01:07:21 +0200

    looked through the whole image & checked checklist

--- app/main.py
+++ app/main.py
@@ -2,16 +2,21 @@ import os
 import requests
 
 
+URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    api_key = os.getenv("API_KEY")
-    response = requests.get(
-        "https://api.weatherapi.com/v1/current.json?"
-        f"key={api_key}&q=Paris"
-    )
+    response = requests.get(url=URL, params={"key": API_KEY, "q": CITY})
+    print("Performing request to Weather API for city Paris...")
     if response.status_code == 200:
         response_json = response.json()
-        print("The weather in Paris is : "
-              f"{response_json['current']['temp_c']} degrees Celsius")
+        print(f"{response_json['location']['name']}"
+              f"/{response_json['location']['country']}"
+              f" {response_json['location']['localtime']}"
+              f" Weather: {response_json['current']['temp_c']} Celsius,"
+              f" {response_json['current']['condition']['text']}")
     else:
         print("An error occurred, try again later!")
 

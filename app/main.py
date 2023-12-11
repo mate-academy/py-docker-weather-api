ed369f75b84af28ecb51fbd4c1e4eab96f2ed987
Lytillis <chemurchem@gmail.com>
2023-12-11 08:28:33 +0200

    Docker Weather API solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_URL = "http://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+API_KEY = os.environ["API_KEY"]
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(
+        API_URL + f"?key={API_KEY}&q={FILTERING}",
+    )
+    data = json.loads(response._content)
+    city = data["location"]["name"]
+    country = data["location"]["country"]
+    datetime = data["location"]["localtime"]
+    temperature = data["current"]["temp_c"]
+    weather = data["current"]["condition"]["text"]
+    print(
+        f"{city}/{country} {datetime} Weather:"
+        f" {temperature} Celsius, {weather}"
+    )
 
 
 if __name__ == "__main__":
+    print("Preforming request to Weather API for cite Paris...")
     get_weather()

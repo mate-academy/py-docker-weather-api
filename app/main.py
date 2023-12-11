b59614c71a7ea92c5fe1b4b4517d6d75bfb95914
Ruslan Misko <miskoruslan6@gmail.com>
2023-12-11 03:21:12 +0200

    First exp with docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {"key": API_KEY, "q": CITY}
+
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+
+        location = weather_data["location"]["name"]
+        temperature = weather_data["current"]["temp_c"]
+        condition = weather_data["current"]["condition"]["text"]
+
+        print(f"Weather in {location}:\nTemperature: {temperature}\n"
+              f"Condition: {condition}")
+    else:
+        print(f"Error: {response.status_code} -> {response.text}")
 
 
 if __name__ == "__main__":

a36cf6b5e0ff5ca020b405fd079bd8af662337f4
wonsky <pidborskii17@gmail.com>
2023-12-12 11:46:13 +0200

    Solution update

--- app/main.py
+++ app/main.py
@@ -13,11 +13,13 @@ API_KEY = os.getenv("API_KEY")
 def get_weather() -> None:
     params = {"key": API_KEY, "q": CITY}
 
+    print(f"Performing request to Weather API for city {CITY}...")
+
     response = requests.get(URL, params=params)
 
     if response.status_code == 200:
         weather_data = response.json()
-        print(f"Current Weather in {CITY}:")
+        print(f"Current Weather in {CITY} ({weather_data['location']['localtime']}):")
         print(f"It is {weather_data['current']['condition']['text']} now")
         print(f"Temperature: {weather_data['current']['temp_c']} Celsius")
     else:

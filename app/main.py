e1a9d0c56fa0a6601aece9873a48c733834dafa7
sakhaline <alexandra.ivva@gmail.com>
2023-12-13 17:07:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,19 +5,24 @@ import json
 from typing import Union
 
 
-def get_weather(api_key: str, city: str = "Paris") -> Union[dict, str]:
-    url = f"http://api.weatherapi.com/v1/current.json?q={city}&key={api_key}"
-    response = requests.get(url)
-    data = response.json()
+URL = "http://api.weatherapi.com/v1/current.json"
+API_KEY = os.environ.get("WEATHER_API_KEY")
+CITY = "Paris"
 
-    return json.dumps(data, indent=2)
+
+def get_weather() -> Union[dict, str]:
+    response = requests.get(
+        URL + f"q={CITY}&key={API_KEY}"
+    ).json()
+
+    print(
+        f"Weather in {CITY}:\n"
+        f"{json.dumps(response, indent=2)}"
+    )
 
 
 if __name__ == "__main__":
-    api_key = os.environ.get("WEATHER_API_KEY")
-
-    if not api_key:
+    if not API_KEY:
         raise ValueError("WEATHER_API_KEY environment variable is not set.")
 
-    weather_data = get_weather(api_key)
-    print(f"Weather in Paris: {weather_data}")
+    get_weather()

a25d6ce5112de82e1e728a8fcb5ca1e73ad39daa
Roman Zhadan <21gehassig@gmail.com>
2023-12-14 02:26:46 +0200

    add CITY a constant to main.py

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ import requests
 
 API_KEY = os.environ.get("API_KEY")
 URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
 
 
 def get_weather() -> None:
-    response = requests.get(URL, params={"key": API_KEY, "q": "Paris"})
+    response = requests.get(URL, params={"key": API_KEY, "q": CITY})
     data = response.json()
 
     location = data["location"]["country"]
@@ -15,7 +16,7 @@ def get_weather() -> None:
     temperature = data["current"]["temperature"]
     condition = data["current"]["condition"]["text"]
 
-    print("Performing request to Weather API for city Paris...")
+    print(f"Performing request to Weather API for city {CITY}...")
     print(f"{location} {time} Weather: {temperature} Celsius, {condition}")
 
 

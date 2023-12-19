d2c2de568ff6ddbd6a8e85ad978e689a6a4abe43
Yaroslav Rybalka <60896665+markus2812@users.noreply.github.com>
2023-12-19 18:05:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,29 +2,29 @@ import os
 import requests
 
 
-BASE_URL = f"https://api.weatherapi.com/v1/current.json"
+BASE_URL = "https://api.weatherapi.com/v1/current.json"
 CITY = "Paris"
 
 
 def get_weather() -> None:
-    api_key = os.getenv('API_KEY')
+    api_key = os.getenv("API_KEY")
     if not api_key:
         raise ValueError("No API_KEY set for Weather API")
-    print(f"Performing request to Weather API for city Paris..")
+    print("Performing request to Weather API for city Paris..")
 
     params = {
-        'key': api_key,
-        'q': CITY,
+        "key": api_key,
+        "q": CITY,
     }
 
     response = requests.get(BASE_URL, params=params)
     weather_data = response.json()
 
-    city_name = weather_data['location']['name']
-    country = weather_data['location']['country']
-    local_time = weather_data['location']['localtime']
-    temp_c = weather_data['current']['temp_c']
-    condition_text = weather_data['current']['condition']['text']
+    city_name = weather_data["location"]["name"]
+    country = weather_data["location"]["country"]
+    local_time = weather_data["location"]["localtime"]
+    temp_c = weather_data["current"]["temp_c"]
+    condition_text = weather_data["current"]["condition"]["text"]
 
     formatted_output = (
         f"{city_name}/{country}"

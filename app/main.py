133facbe30ece69f6ba3636bdfa85d0ad9c6c423
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-12-22 20:38:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import requests
+from datetime import datetime
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    base_url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": os.environ.get("API_KEY"),
+        "q": "Paris",
+    }
+
+    current_datetime = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+
+        weather_data = response.json()
+
+        location = weather_data["location"]["name"]
+        temperature_celsius = weather_data["current"]["temp_c"]
+        condition = weather_data["current"]["condition"]["text"]
+
+        print(f"Weather in {location} at {current_datetime}:")
+        print(f"Temperature: {temperature_celsius}°C")
+        print(f"Condition: {condition}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
 
 
 if __name__ == "__main__":

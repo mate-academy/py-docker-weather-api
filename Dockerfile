63bbdd47fd40158343cc81d4c21a7e374bede6d7
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-01-19 13:49:11 +0200

    Add solution

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10.4
+LABEL maintainer="ilya@gmail.com"
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    city = "Paris"
+    text = requests.get(
+        f"https://api.weatherapi.com/v1/current.json"
+        f"?key={os.getenv('API_KEY')}&q={city}&aqi=yes"
+    ).json()
+    print(
+        f"Now the weather in {city} is "
+        f"{text['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":

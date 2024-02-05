5f0afbb39b8812e41ba96d86e44f9c1091ff6992
ender <endernagshushman@gmail.com>
2024-02-05 19:41:43 +0200

    fixes

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ import os
 def get_weather() -> None:
     req = requests.get(f"http://api.weatherapi.com/v1/current.json?"
                        f"key={os.getenv('API_KEY')}&q=Paris&aqi=yes")
-    print("Performing request to Weather API for city Paris...")
-    print(f"Paris/France"
-          f" {req.json().get('location')['localtime']}"
-          f" Weather: {req.json().get('current').get('temp_c')}"
-          f" Celsius, {req.json().get('current').get('condition').get('text')}"
-          )
+    if req.status_code == 200:
+        print("Performing request to Weather API for city Paris...")
+        print(f"Paris/France"
+              f" {req.json().get('location')['localtime']}"
+              f" Weather: {req.json().get('current').get('temp_c')}"
+              f" Celsius, {req.json().get('current').get('condition').get('text')}"
+              )
 
 
 if __name__ == "__main__":

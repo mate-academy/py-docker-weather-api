a0bec3edd7048c2f6a66fbb68080c3a362981a99
Ivan <mozharivan17@gmail.com>
2024-02-13 04:05:55 +0200

    Add blank lines to dockerignore and requirements, create separate functions for parsing json

--- app/main.py
+++ app/main.py
@@ -7,17 +7,21 @@ def get_weather() -> None:
     api_key = os.getenv("API_KEY")
     url = "http://api.weatherapi.com/v1/current.json"
     filtering = "Paris"
+    params = {
+        "key": api_key,
+        "g": filtering
+    }
 
-    result = requests.get(url + f"?key={api_key}&q={filtering}")
+    result = requests.get(url, params=params)
 
     weather_data = result.json()
-    location = (weather_data["location"]["name"]
-                + "/"
-                + weather_data["location"]["country"])
+    city = weather_data["location"]["name"]
+    country = weather_data["location"]["country"]
     date = weather_data["location"]["localtime"]
-    temperature, condition = (weather_data["current"]["temp_c"],
-                              weather_data["current"]["condition"]["text"])
-    print(f"{location} {date} Weather: {temperature} Celsius, {condition}")
+    temperature = weather_data["current"]["temp_c"]
+    condition = weather_data["current"]["condition"]["text"]
+    print(f"{city}/{country} {date} "
+          f"Weather: {temperature} Celsius, {condition}")
 
 
 if __name__ == "__main__":

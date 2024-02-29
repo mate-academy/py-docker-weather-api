c20e7e54ff25aa460404d59e02fd6306d88dbc5a
Zonda YT <zondayttop1ggg@gmail.com>
2024-02-29 21:07:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
 
+API_KEY = os.environ.get("API_KEY")
+CITY = "Lviv"
+
+WEATHER_API_URL = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}"
+
+def get_weather() -> None:
+    response = requests.get(WEATHER_API_URL)
+    data = response.json()
+    return data
 
 if __name__ == "__main__":
-    get_weather()
+    weather_info = get_weather()
+    print(weather_info)

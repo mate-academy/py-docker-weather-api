ade7018c1751da3f32cff3ac51b3a0ea7f57ed42
3lobites <3lobites@gmail.com>
2024-03-04 16:37:29 +0200

    vklyuchil mozgi

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-def get_weather() -> None:
-    URL = "http://api.weatherapi.com/?"
-    FILTERING = "Paris"
+from __future__ import print_function
+import requests
 
-    result = requests.get(URL + f"q={FILTERING}")
+
+# http://api.weatherapi.com/v1/search.json?key=1a89060c76a14afba95134309242702&q=Paris
+
+def get_weather() -> None:
+    url = "http://api.weatherapi.com/v1/current.json?"
+    work_key = "1a89060c76a14afba95134309242702"
+    work_sity = "Paris"
+    work_params = f"key={work_key}&q={work_sity}"
+
+    work_request = requests.get(url, params=work_params)
+
+    if work_request.status_code != 200:
+        print(f"Error request: {work_request.status_code}")
+
+    result = work_request.json()
+
+    test_view = f"Local time: {result['location']['localtime']}, temp(C): {result['current']['temp_c']}, precipitation: {result['current']['condition']['text']}, wind(m/c): {result['current']['wind_mph']}, direction wind: {result['current']['wind_dir']}"
+
+    ttt = 0
 
 
 if __name__ == "__main__":

90b1801db353eda817cb2d085d6cc12bcdeaf1f8
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-03-11 18:25:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -12,13 +12,11 @@ def get_weather() -> None:
     request = requests.get(f"{url}?key={api_key}&q={filter_city}&aqi=no")
     if request.status_code == 200:
         data = request.json()
-        print(f"Request to Weather API for city {filter_city}")
-        print(
-            f"{data["location"]["name"]}, {data["location"]["country"]} "
-            f"{data["location"]["localtime"]} "
-            f"Weather: {data["current"]["temp_c"]} Celsius, "
-            f"{data["current"]["condition"]["text"]}"
-        )
+        print(f'Performing request to Weather API for city {filter_city}...')
+        print(f'{data["location"]["name"]}/{data["location"]["country"]} '
+              f'{data["location"]["localtime"]} '
+              f'Weather: {data["current"]["temp_c"]} Celsius, '
+              f'{data["current"]["condition"]["text"]}')
     else:
         print("Something went wrong")
 

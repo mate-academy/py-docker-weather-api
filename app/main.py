10e7a6b1c9d034d80bd7c2d5b88325317e179fd4
Yuliana <pashkova.yuliana@gmail.com>
2024-03-12 13:45:00 +0200

    add function to print current weather in chosen city

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+import os
+
+import requests
+
+
+url = "http://api.weatherapi.com/v1/current.json"
+filter_city = "Paris"
+api_key = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    response = requests.get(f"{url}?key={api_key}&q={filter_city}&aqi=no")
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"City: {data['location']['name']}/{data['location']['country']}")
+        print(f"Local Time: {data['location']['localtime']}")
+        print(f"Weather: {data['current']['temp_c']} Celsius, {data['current']['condition']['text']}")
+    else:
+        print(f"Error: {response.status_code}. Something went wrong with the Weather API request.")
 
 
 if __name__ == "__main__":

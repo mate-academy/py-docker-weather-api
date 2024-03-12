1a360853d2447cf50f97e6767b87a7c8fc143805
Yuliana <pashkova.yuliana@gmail.com>
2024-03-12 13:47:16 +0200

    fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -14,11 +14,14 @@ def get_weather() -> None:
 
     if response.status_code == 200:
         data = response.json()
-        print(f"City: {data['location']['name']}/{data['location']['country']}")
+        print(f"City: {data['location']['name']}/"
+              f"{data['location']['country']}")
         print(f"Local Time: {data['location']['localtime']}")
-        print(f"Weather: {data['current']['temp_c']} Celsius, {data['current']['condition']['text']}")
+        print(f"Weather: {data['current']['temp_c']} Celsius, "
+              f"{data['current']['condition']['text']}")
     else:
-        print(f"Error: {response.status_code}. Something went wrong with the Weather API request.")
+        print(f"Error: {response.status_code}. "
+              f"Something went wrong with the Weather API request.")
 
 
 if __name__ == "__main__":

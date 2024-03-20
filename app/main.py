1d003cd79774291fa581b6c2bb1db6d57c0016e2
Artur Poltser <polcerartur@gmail.com>
2024-03-20 11:30:23 +0200

    add correct exceptions handling

--- app/main.py
+++ app/main.py
@@ -3,24 +3,27 @@ from datetime import datetime
 
 import requests
 
-API_KEY = os.environ.get("API_KEY")
+API_KEY = os.getenv("API_KEY")
 CITY = "Paris"
-URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}"
+URL = f"http://api.weatherapi.com/v1/current.json"
 
 
 def get_weather() -> None:
-    result = requests.get(URL)
-    result.raise_for_status()
-    data = result.json()["current"]
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-    weather_content = (
-        f"Performing request to Weather API for city {CITY}...\n"
-        f"{CITY}/France {current_time}"
-        f" Weather: {data['temp_c']} Celsius, "
-        f"{data['condition']['text']}"
-    )
+    try:
+        result = requests.get(URL, params={"key": API_KEY, "q": CITY})
+        result.raise_for_status()
+        data = result.json()["current"]
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+        weather_content = (
+            f"Performing request to Weather API for city {CITY}...\n"
+            f"{CITY}/France {current_time}"
+            f" Weather: {data['temp_c']} Celsius, "
+            f"{data['condition']['text']}"
+        )
 
-    print(weather_content)
+        print(weather_content)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":

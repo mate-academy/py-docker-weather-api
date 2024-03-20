c2edb26819be46adadda1519d6dfc94224c46114
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-20 20:37:01 +0200

    implement getting weather from weather API and set up requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import requests
+import json
+import dotenv
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    dotenv.load_dotenv()
+    res = requests.get(
+        f"http://api.weatherapi.com/v1/current.json?key={os.getenv('API_KEY')}&q=Paris"
+    )
+    res = json.loads(res.content)
+    print(
+        f"Performing request to Weather API for city Paris:\n"
+        f"Paris/France {res['location']['localtime']} Weather: "
+        f"{res['current']['temp_c']} Celsius, "
+        f"{res['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":

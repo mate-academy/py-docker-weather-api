65fac688ee671a0e147060b351ba79ecbf069e76
Heorhii Nadiradze <swain.volt@gmail.com>
2024-03-21 08:22:13 +0200

    develop

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ import requests
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
 
 
 def get_weather() -> None:
     weather_url = "http://api.weatherapi.com/v1"
     current_weather = "/current.json"
-    city = "Paris"
-    url = f"{weather_url}{current_weather}?key={API_KEY}&q={city}"
+    url = f"{weather_url}{current_weather}?key={API_KEY}&q={CITY}"
     response = requests.get(url).json()
     print(response)
     print(response["location"]["name"])

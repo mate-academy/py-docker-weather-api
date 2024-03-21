9d1af8cd65ce8f0196bd830b6938da9bf9342745
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-21 18:31:15 +0200

    implement verification of response status code

--- app/main.py
+++ app/main.py
@@ -10,13 +10,16 @@ def get_weather() -> None:
         f"http://api.weatherapi.com/v1/current.json?key="
         f"{os.getenv('API_KEY')}&q=Paris"
     )
-    res = json.loads(res.content)
-    print(
-        f"Performing request to Weather API for city Paris:\n"
-        f"Paris/France {res['location']['localtime']} Weather: "
-        f"{res['current']['temp_c']} Celsius, "
-        f"{res['current']['condition']['text']}"
-    )
+    if res.status_code == 200:
+        res = json.loads(res.content)
+        print(
+            f"Performing request to Weather API for city Paris:\n"
+            f"Paris/France {res['location']['localtime']} Weather: "
+            f"{res['current']['temp_c']} Celsius, "
+            f"{res['current']['condition']['text']}"
+        )
+    else:
+        print("Request was not successful. Status code: " + str(res.status_code))
 
 
 if __name__ == "__main__":

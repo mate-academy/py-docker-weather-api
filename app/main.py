d89169b9f5b352effafb391e46a4dd5ad57263e4
kabust <tulyulyuk91@gmail.com>
2024-03-24 12:07:18 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,40 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+from requests import Response
+
+
+API_KEY = os.environ.get("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json?"
+
+
+def get_weather(city: str = "Paris") -> Response:
+    print(f"Requesting {city} weather from Weather API...")
+    r = requests.get(
+        URL + f"key={API_KEY}&q={city}&aqi=no"
+    ).json()
+
+    if "error" in r:
+        print(r["error"]["message"])
+        return r
+
+    location = r["location"]
+    current_temp = r["current"]
+    condition = current_temp["condition"]
+    print("▼" * 50)
+    print(
+        f"{location["name"]}/{location["country"]} {location["localtime"]}\n"
+        f"Updated at: {current_temp["last_updated"]} \n"
+        f"Weather: {current_temp["temp_c"]} C (feels like {current_temp["feelslike_c"]} C), {condition["text"]}"
+    )
+    print("▲" * 50)
+
+    return r
 
 
 if __name__ == "__main__":
-    get_weather()
+    city = input("Provide city (or leave empty for Paris): ")
+
+    if city:
+        get_weather(city=city)
+    else:
+        get_weather()

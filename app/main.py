588129b21955210611b465c057060bfc8e7cf4c5
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-25 18:27:02 +0200

    style: Fix flake8 error

--- app/main.py
+++ app/main.py
@@ -10,10 +10,12 @@ API_KEY = os.getenv("API_KEY", None)
 def get_weather() -> None:
     print(f"Performing request to Weather API for city {FILTERING}")
     response = requests.get(URL + f"?q={FILTERING}&key={API_KEY}").json()
-    location = response["location"].get("name") + "/" + response["location"].get("country")
+    location = (f"{response["location"].get("name")}/"
+                + response["location"].get("country"))
     localtime = response["location"].get("localtime")
     temperature = response["current"].get("temp_c")
     condition = response["current"]["condition"].get("text")
+
     print(
         f"{location} {localtime} Weather: {temperature} Celsius, {condition}"
     )

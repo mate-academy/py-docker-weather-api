68f41b09ebdb2b1296fd2bca2a3dd5fdbf3dac5a
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-25 18:54:07 +0200

    style: Fix quotes in main.py

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ API_KEY = os.getenv("API_KEY", None)
 def get_weather() -> None:
     print(f"Performing request to Weather API for city {FILTERING}")
     response = requests.get(URL + f"?q={FILTERING}&key={API_KEY}").json()
-    location = (f"{response["location"].get("name")}/"
+    location = (f"{response['location'].get('name')}/"
                 + response["location"].get("country"))
     localtime = response["location"].get("localtime")
     temperature = response["current"].get("temp_c")

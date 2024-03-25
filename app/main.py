ddf06e69e5902aebaa833fd40650ae3f7e186e57
Maksym Operchuk <operlast529@gmail.com>
2024-03-25 16:25:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,19 +8,17 @@ FILTERING = "Kyiv"
 
 def get_weather() -> str:
 
-    response = requests.get(
-        URL + f"key={os.environ.get("API_KEY")}&q={FILTERING}"
-    )
+    res = requests.get(URL + f"key={os.environ.get("API_KEY")}&q={FILTERING}")
 
-    if response.status_code == 200:
-        data = response.json()
+    if res.status_code == 200:
+        data = res.json()
         location = data["location"]["name"]
         temperature = data["current"]["temp_c"]
 
         print(f"location: {location}, temperature: {temperature}")
         return data
     else:
-        print("Failed to fetch data:", response.status_code)
+        print("Failed to fetch data:", res.status_code)
 
 
 if __name__ == "__main__":

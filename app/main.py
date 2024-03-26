0c46a7f9c1c6b8c7fc0e9890ecc7b35fdcd312cc
barabarinov <barabarinov@gmail.com>
2024-03-26 10:46:32 +0200

    fixed according to flake8

--- app/main.py
+++ app/main.py
@@ -30,7 +30,9 @@ def get_weather() -> None:
             f"Feels like °C: {response_current.get('feelslike_c')}\n"
         )
     else:
-        print(f"Status code: {response.status_code}, Reason: {response.reason}")
+        print(
+            f"Status code: {response.status_code}, Reason: {response.reason}"
+        )
 
 
 if __name__ == "__main__":

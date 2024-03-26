00d67932202330d13cd1769cdfcc4a78942231c1
Mykola <mykola22dubyna@gmail.com>
2024-03-26 10:15:54 +0200

    add error handling feature

--- app/main.py
+++ app/main.py
@@ -9,7 +9,12 @@ FILTERING = "Paris"
 
 def get_weather() -> None:
     print("Performing request to Weather API for city Paris...")
-    result = requests.get(URL + f"key={API_KEY}&q={FILTERING}").json()
+    result = requests.get(URL + f"key={API_KEY}&q={FILTERING}")
+    if result.status_code != requests.codes.ok:
+        print(f"Request failed with status code {result.status_code}\n"
+              f"{result.text}")
+
+    result = result.json()
     city = result["location"]["name"]
     country = result["location"]["country"]
     localtime = result["location"]["localtime"]

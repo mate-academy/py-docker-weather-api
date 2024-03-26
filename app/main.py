499242d2e1d624c8ba486a3a041b2ca9aee7601b
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-26 20:00:30 +0200

    feat: Add exceptions handling

--- app/main.py
+++ app/main.py
@@ -9,16 +9,22 @@ API_KEY = os.getenv("API_KEY", None)
 
 def get_weather() -> None:
     print(f"Performing request to Weather API for city {FILTERING}")
-    response = requests.get(URL, params={"key": API_KEY, "q": FILTERING}).json()
-    location = (f"{response['location'].get('name')}/"
-                + response["location"].get("country"))
-    localtime = response["location"].get("localtime")
-    temperature = response["current"].get("temp_c")
-    condition = response["current"]["condition"].get("text")
+    try:
+        response = requests.get(
+            URL, params={"key": API_KEY, "q": FILTERING}
+        ).json()
+        location = (f"{response["location"].get("name")}/"
+                    + response["location"].get("country"))
+        localtime = response["location"].get("localtime")
+        temperature = response["current"].get("temp_c")
+        condition = response["current"]["condition"].get("text")
 
-    print(
-        f"{location} {localtime} Weather: {temperature} Celsius, {condition}"
-    )
+        print(
+            f"{location} {localtime} Weather: "
+            f"{temperature} Celsius, {condition}"
+        )
+    except Exception:
+        print("Error! Something went wrong!")
 
 
 if __name__ == "__main__":

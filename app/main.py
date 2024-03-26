d3f50144bbcaa7de4f9fe07f078632ea9bf6e13c
Tymofii Skopenko <youmustdeth@gmail.com>
2024-03-26 15:34:04 +0200

    using params & create error handaling

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ API_KEY = os.getenv("API_KEY")
 
 def get_weather() -> None:
     print(f"Performing request to Weather API for city {FILTERING}...")
-    result = requests.get(URL + f"q={FILTERING}&key={API_KEY}")
-    if result.status_code == 200:
+    result = requests.get(URL, params={"key": API_KEY, "q": FILTERING})
+    if result.status_code == requests.codes.ok:
         data = result.json()
         city = data.get("location").get("name")
         country = data.get("location").get("country")
@@ -17,6 +17,8 @@ def get_weather() -> None:
         temperature = data.get("current").get("temp_c")
         condition = data.get("current").get("condition").get("text")
         print(f"{location} {date} Weather: {temperature} Celsius, {condition}")
+    else:
+        print("Something goes wrong, check your connection")
 
 
 if __name__ == "__main__":

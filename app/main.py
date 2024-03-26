c7392f8f27d0b64b5a553af7bdb29a5fef0f9a29
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-03-26 14:14:51 +0200

    Sol

--- app/main.py
+++ app/main.py
@@ -2,28 +2,31 @@ import os
 
 import requests
 
-# os.environ["API_KEY"] = "7c50588873ce40198ea125550242503"
-API_KEY = os.environ.get("API_KEY")
+
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
 URL = "https://api.weatherapi.com/v1/current.json"
 
 
 def get_weather() -> None:
-    params = {"key": API_KEY, "q": "Paris"}
+    params = {"key": API_KEY, "q": CITY}
 
-    data = requests.get(URL, params).json()
+    try:
+        data = requests.get(URL, params).json()
+        country = data["location"]["country"]
+        time = data["current"]["last_updated"]
+        temperature = data["current"]["temp_c"]
+        weather = data["current"]["condition"]["text"]
 
-    country = data["location"]["country"]
-    time = data["current"]["last_updated"]
-    temperature = data["current"]["temp_c"]
-    weather = data["current"]["condition"]["text"]
-
-    print(
-        f"Country: {country}\n"
-        f"City: Paris\n"
-        f"Time: {time}\n"
-        f"Temperature: {temperature}\n"
-        f"Weather: {weather}"
-    )
+        print(
+            f"Country: {country}\n"
+            f"City: Paris\n"
+            f"Time: {time}\n"
+            f"Temperature: {temperature}\n"
+            f"Weather: {weather}"
+        )
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
 
 
 if __name__ == "__main__":

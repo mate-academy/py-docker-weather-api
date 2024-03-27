445a2c52ecf0e4ac7213087f6102bb8bd6956443
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-28 00:51:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+
+
+def get_weather(query: str) -> None:
+    print("Performing request to Weather API for city Paris...")
+    api_key = os.environ.get(
+        "API_KEY",
+        "45aa589d47f145f3874211504242703"
+    )
+    responce = requests.post(
+        f"https://api.weatherapi.com/v1/current.json?"
+        f"key={api_key}&q={query}&aqi=no/"
+    )
+    current = responce.json()["current"]
+    location = responce.json()["location"]
+    print(f"{location['tz_id']} {current['last_updated']} "
+          f"Weather: {current['temp_c']} Celsius!!!")
 
 
 if __name__ == "__main__":
-    get_weather()
+    get_weather("Paris")

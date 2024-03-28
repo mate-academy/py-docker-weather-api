796a2aeab493e7ba01add29a150c81dc4153afbd
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-03-28 16:20:34 +0200

    Solution

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.9-alpine
 LABEL maintainer="yaroslavbiziuk@gmail.com"
 
 ENV PYTHONUNBUFFERED 1
+ENV API_KEY 45aa589d47f145f3874211504242703
 
 WORKDIR app
 
--- app/main.py
+++ app/main.py
@@ -11,10 +11,11 @@ def get_weather(query: str) -> None:
         f"https://api.weatherapi.com/v1/current.json?"
         f"key={api_key}&q={query}&aqi=no/"
     )
-    current = responce.json()["current"]
-    location = responce.json()["location"]
-    print(f"{location['tz_id']} {current['last_updated']} "
-          f"Weather: {current['temp_c']} Celsius!!!")
+    if responce.status_code == 200:
+        current = responce.json()["current"]
+        location = responce.json()["location"]
+        print(f"{location['tz_id']} {current['last_updated']} "
+              f"Weather: {current['temp_c']} Celsius!!!")
 
 
 if __name__ == "__main__":
